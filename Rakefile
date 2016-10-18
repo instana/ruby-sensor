d3c25e61d2398a311f31482fac2d748da57ff6cb
Peter Giacomo Lombardo <pglombardo@gmail.com>
2016-10-18 16:34:47 +0200

    First iteration of bundled tests.

--- Rakefile
+++ Rakefile
@@ -2,6 +2,10 @@ require "bundler/gem_tasks"
 require "rake/testtask"
 
 Rake::TestTask.new(:test) do |t|
+  t.verbose = false
+  t.warning = false
+  t.ruby_opts = []
+
   t.libs << "test"
   t.libs << "lib"
   t.test_files = FileList['test/**/*_test.rb']
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,13 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'instana'
 
-require 'minitest/autorun'
+require 'rubygems'
+require 'bundler/setup'
+require "minitest/spec"
+require "minitest/autorun"
+require "minitest/reporters"
+require "minitest/debugger" if ENV['DEBUG']
+
+Minitest::Reporters.use! MiniTest::Reporters::SpecReporter.new
+
+Bundler.require(:default, :test)
