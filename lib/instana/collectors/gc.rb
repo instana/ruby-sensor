32e611da3a48bd52bc5db78ab6e0df2937c0815f
Peter Giacomo Lombardo <pglombardo@gmail.com>
2016-10-21 11:37:23 +0200

    Better delta reporting for GC stats.

--- lib/instana/collectors/gc.rb
+++ lib/instana/collectors/gc.rb
@@ -3,11 +3,15 @@ module Instana
     class GC
       attr_accessor :last_minor_count
       attr_accessor :last_major_count
+      attr_accessor :last_minor_reported
+      attr_accessor :last_major_reported
 
       def initialize
+        @first_run = true
         @last_minor_count = 0
         @last_major_count = 0
         ::Instana.agent.payload[:gc] = { :minorGcs => 0, :majorGcs => 0 }
+        ::GC::Profiler.enable
       end
 
       ##
@@ -25,20 +29,31 @@ module Instana
         cur_minor_count = stats[:minor_gc_count]
         cur_major_count = stats[:major_gc_count]
 
+        # If this is the first run, just set these values
+        # so zero will be reported for the first run
+        if @first_run
+          @last_minor_count = cur_minor_count
+          @last_major_count = cur_major_count
+          @first_run = false
+          return
+        end
+
         minor_diff = cur_minor_count - @last_minor_count
         major_diff = cur_major_count - @last_major_count
 
         # Report _only_ when the value has changed from
         # the last time around the carousel
-        if minor_diff == last_gc[:minorGcs]
+        if minor_diff == @last_minor_reported
           this_gc.delete(:minorGcs)
         else
+          @last_minor_reported = minor_diff
           this_gc[:minorGcs] = minor_diff
         end
 
-        if major_diff == last_gc[:majorGcs]
+        if major_diff == @last_major_reported
           this_gc.delete(:majorGcs)
         else
+          @last_major_reported = major_diff
           this_gc[:majorGcs] = major_diff
         end
 
