85ed1e760869c0a3dc05580216cd242811249cb1
pglombardo <pglombardo@gmail.com>
2016-10-24 15:27:35 +0200

    Use fully qualified namespace calls.

--- lib/instana/collectors.rb
+++ lib/instana/collectors.rb
@@ -12,27 +12,26 @@ module Instana
   end
 end
 
-Instana.pry!
 if ENV.key?('INSTANA_GEM_DEV')
   ::Instana::Collector.interval = 5
 else
   ::Instana::Collector.interval = 1
 end
 
-Instana.collectors << ::Instana::Collector::GC.new
-Instana.collectors << ::Instana::Collector::Heap.new
-Instana.collectors << ::Instana::Collector::Memory.new
-Instana.collectors << ::Instana::Collector::Thread.new
+::Instana.collectors << ::Instana::Collector::GC.new
+::Instana.collectors << ::Instana::Collector::Heap.new
+::Instana.collectors << ::Instana::Collector::Memory.new
+::Instana.collectors << ::Instana::Collector::Thread.new
 
-Thread.new do
-  timers = Timers::Group.new
+::Thread.new do
+  timers = ::Timers::Group.new
   timers.every(::Instana::Collector.interval) {
     ::Instana.collectors.each do |c|
       c.collect
     end
 
     # Report all the collected goodies
-    Instana.agent.report_entity_data
+    ::Instana.agent.report_entity_data
   }
   loop { timers.wait }
 end
