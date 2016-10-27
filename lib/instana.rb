92dd3bb1ccc5140f85cab49b84603532191f4848
pglombardo <pglombardo@gmail.com>
2016-10-27 12:00:24 +0200

    Move initialization to a start method.

--- lib/instana.rb
+++ lib/instana.rb
@@ -9,20 +9,38 @@ module Instana
     attr_accessor :collectors
     attr_accessor :config
     attr_accessor :logger
+    attr_accessor :pid
+
+    ##
+    # start
+    #
+    # Initialize the Instana language agent
+    #
+    def start
+      Instana.agent = Instana::Agent.new
+      Instana.collectors = []
+      Instana.logger = Logger.new(STDOUT)
+      Instana.logger.info "Stan is on the scene.  Starting Instana instrumentation."
+
+      # Store the current pid so we can detect a potential fork
+      # later on
+      Instana.pid = Process.pid
+    end
+
+    def pid_change?
+      @pid != Process.pid
+    end
   end
 end
 
 require "instana/config"
 require "instana/agent"
 
-Instana.agent = Instana::Agent.new
-Instana.collectors = []
-Instana.logger = Logger.new(STDOUT)
-Instana.logger.info "Stan is on the scene.  Starting Instana instrumentation."
+::Instana.start
 
-if Instana.agent.host_agent_ready?
-  Instana.agent.announce_sensor
+if ::Instana.agent.host_agent_ready?
+  ::Instana.agent.announce_sensor
   require "instana/collectors"
 else
-  Instana.logger.debug "Instana host agent not available.  Going to sit in a corner quietly."
+  ::Instana.logger.info "Instana host agent not available.  Going to sit in a corner quietly."
 end
