c413d445d80f00686ebbfabb2c80852da9333a45
Peter Giacomo Lombardo <pglombardo@gmail.com>
2016-11-10 17:52:19 +0100

    Better queue processing and report of traces

--- lib/instana/tracing/processor.rb
+++ lib/instana/tracing/processor.rb
@@ -23,8 +23,27 @@ module Instana
     # Sends all traces in @queue to the host
     # agent
     #
+    # FIXME: Add limits checking here in regards to:
+    #   - Max HTTP Post size
+    #   - Out of control/growing queue
+    #   - Prevent another run of the timer while this is running
+    #
     def send
       return if @queue.empty?
+
+      size = @queue.size
+      if size > 10
+        Instana.logger.debug "Trace queue is #{size}"
+      end
+
+      spans = []
+      until @queue.empty? do
+        set = @queue.pop(true)
+        set.each do |s|
+          spans << s.raw
+        end
+      end
+      ::Instana.agent.report_traces(spans)
     end
   end
 end
