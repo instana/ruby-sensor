1ad67ef5a876239b54346eef9791338bb1ec3f3f
Peter Giacomo Lombardo <pglombardo@gmail.com>
2016-11-17 22:13:55 +0100

    Send basic HTTP KVs in rack

--- lib/instana/instrumentation/rack.rb
+++ lib/instana/instrumentation/rack.rb
@@ -5,14 +5,22 @@ module Instana
     end
 
     def call(env)
+      req = ::Rack::Request.new(env)
+      kvs = { :http => {} }
+      kvs[:http][:method] = req.request_method
+      kvs[:http][:url] = ::CGI.unescape(req.path)
       ::Instana.tracer.log_start_or_continue(:rack)
 
-      @app.call(env)
+      status, headers, response = @app.call(env)
+
+      kvs[:http][:status] = status
+
+      [status, headers, response]
     rescue Exception => e
       ::Instana.tracer.log_error(e)
       raise
     ensure
-      ::Instana.tracer.log_end(:rack)
+      ::Instana.tracer.log_end(:rack, kvs)
     end
   end
 end
