986389eeaf71a166b38521e29f459b31db42e8c5
Peter Giacomo Lombardo <pglombardo@gmail.com>
2016-11-18 11:55:21 +0100

    Add/updated tests to validate traces and spans

--- test/rack_test.rb
+++ test/rack_test.rb
@@ -1,5 +1,6 @@
 require 'rack/test'
 require 'rack/lobster'
+require "instana/rack"
 
 class RackTest < Minitest::Test
   include Rack::Test::Methods
@@ -18,5 +19,66 @@ class RackTest < Minitest::Test
   def test_basic_get
     get '/mrlobster'
     assert last_response.ok?
+
+    spans = ::Instana.processor.queued_spans
+
+    # Span validation
+    assert_equal 1, spans.count
+    first_span = spans.first
+    assert_equal :rack, first_span[:n]
+    assert_equal :ruby, first_span[:ta]
+    assert first_span.key?(:data)
+    assert first_span[:data].key?(:http)
+    assert_equal "GET", first_span[:data][:http][:method]
+    assert_equal "/mrlobster", first_span[:data][:http][:url]
+    assert_equal 200, first_span[:data][:http][:status]
+    assert first_span.key?(:f)
+    assert first_span[:f].key?(:e)
+    assert first_span[:f].key?(:h)
+    assert_equal ::Instana.agent.agent_uuid, first_span[:f][:h]
+  end
+
+  def test_basic_post
+    post '/mrlobster'
+    assert last_response.ok?
+
+    spans = ::Instana.processor.queued_spans
+
+    # Span validation
+    assert_equal 1, spans.count
+    first_span = spans.first
+    assert_equal :rack, first_span[:n]
+    assert_equal :ruby, first_span[:ta]
+    assert first_span.key?(:data)
+    assert first_span[:data].key?(:http)
+    assert_equal "POST", first_span[:data][:http][:method]
+    assert_equal "/mrlobster", first_span[:data][:http][:url]
+    assert_equal 200, first_span[:data][:http][:status]
+    assert first_span.key?(:f)
+    assert first_span[:f].key?(:e)
+    assert first_span[:f].key?(:h)
+    assert_equal ::Instana.agent.agent_uuid, first_span[:f][:h]
+  end
+
+  def test_basic_put
+    put '/mrlobster'
+    assert last_response.ok?
+
+    spans = ::Instana.processor.queued_spans
+
+    # Span validation
+    assert_equal 1, spans.count
+    first_span = spans.first
+    assert_equal :rack, first_span[:n]
+    assert_equal :ruby, first_span[:ta]
+    assert first_span.key?(:data)
+    assert first_span[:data].key?(:http)
+    assert_equal "PUT", first_span[:data][:http][:method]
+    assert_equal "/mrlobster", first_span[:data][:http][:url]
+    assert_equal 200, first_span[:data][:http][:status]
+    assert first_span.key?(:f)
+    assert first_span[:f].key?(:e)
+    assert first_span[:f].key?(:h)
+    assert_equal ::Instana.agent.agent_uuid, first_span[:f][:h]
   end
 end
--- test/tracer_test.rb
+++ test/tracer_test.rb
@@ -10,9 +10,22 @@ class TracerTest < Minitest::Test
     ::Instana.tracer.start_or_continue_trace(:test_trace, {:one => 1}) do
       sleep 0.5
     end
-    t = ::Instana.tracer.instance_variable_get(:@trace)
+
+    traces = ::Instana.processor.queued_traces
+    assert_equal 1, traces.count
+    t = traces.first
     assert_equal 1, t.spans.size
     assert t.valid?
+
+    first_span = t.spans.first
+    assert_equal :test_trace, first_span[:n]
+    assert_equal :ruby, first_span[:ta]
+    assert first_span.key?(:data)
+    assert_equal 1, first_span[:data][:one]
+    assert first_span.key?(:f)
+    assert first_span[:f].key?(:e)
+    assert first_span[:f].key?(:h)
+    assert_equal ::Instana.agent.agent_uuid, first_span[:f][:h]
   end
 
   def test_errors_are_properly_propogated
@@ -27,8 +40,21 @@ class TracerTest < Minitest::Test
 
     assert exception_raised
 
-    t = ::Instana.tracer.instance_variable_get(:@trace)
+    traces = ::Instana.processor.queued_traces
+    assert_equal 1, traces.count
+    t = traces.first
+    assert_equal 1, t.spans.size
     assert t.valid?
+
+    first_span = t.spans.first
+    assert_equal :test_trace, first_span[:n]
+    assert_equal :ruby, first_span[:ta]
+    assert first_span.key?(:data)
+    assert_equal 1, first_span[:data][:one]
+    assert first_span.key?(:f)
+    assert first_span[:f].key?(:e)
+    assert first_span[:f].key?(:h)
+    assert_equal ::Instana.agent.agent_uuid, first_span[:f][:h]
     assert t.has_error?
   end
 
@@ -39,7 +65,10 @@ class TracerTest < Minitest::Test
         sleep 0.2
       end
     end
-    t = ::Instana.tracer.instance_variable_get(:@trace)
+
+    traces = ::Instana.processor.queued_traces
+    assert_equal 1, traces.count
+    t = traces.first
     assert_equal 2, t.spans.size
     assert t.valid?
   end
@@ -48,7 +77,10 @@ class TracerTest < Minitest::Test
     ::Instana.tracer.log_start_or_continue(:test_trace, {:one => 1})
     ::Instana.tracer.log_info({:info_logged => 1})
     ::Instana.tracer.log_end(:test_trace, {:close_one => 1})
-    t = ::Instana.tracer.instance_variable_get(:@trace)
+
+    traces = ::Instana.processor.queued_traces
+    assert_equal 1, traces.count
+    t = traces.first
     assert_equal 1, t.spans.size
     assert t.valid?
   end
@@ -62,9 +94,23 @@ class TracerTest < Minitest::Test
     ::Instana.tracer.log_exit(:sub_task, {:sub_task_exit_info => 1})
 
     ::Instana.tracer.log_end(:test_trace, {:close_one => 1})
-    t = ::Instana.tracer.instance_variable_get(:@trace)
+
+    traces = ::Instana.processor.queued_traces
+    assert_equal 1, traces.count
+
+    t = traces.first
     assert_equal 2, t.spans.size
     assert t.valid?
+
+    first_span = t.spans.first
+    assert_equal :test_trace, first_span[:n]
+    assert_equal :ruby, first_span[:ta]
+    assert first_span.key?(:data)
+    assert_equal 1, first_span[:data][:one]
+    assert first_span.key?(:f)
+    assert first_span[:f].key?(:e)
+    assert first_span[:f].key?(:h)
+    assert_equal ::Instana.agent.agent_uuid, first_span[:f][:h]
   end
 
   def test_block_tracing_error_capture
@@ -79,7 +125,10 @@ class TracerTest < Minitest::Test
 
     assert exception_raised
 
-    t = ::Instana.tracer.instance_variable_get(:@trace)
+    traces = ::Instana.processor.queued_traces
+    assert_equal 1, traces.count
+
+    t = traces.first
     assert_equal 1, t.spans.size
     assert t.valid?
     assert t.has_error?
@@ -90,7 +139,11 @@ class TracerTest < Minitest::Test
     ::Instana.tracer.log_info({:info_logged => 1})
     ::Instana.tracer.log_error(Exception.new("Low level tracing api error"))
     ::Instana.tracer.log_end(:test_trace, {:close_one => 1})
-    t = ::Instana.tracer.instance_variable_get(:@trace)
+
+    traces = ::Instana.processor.queued_traces
+    assert_equal 1, traces.count
+
+    t = traces.first
     assert_equal 1, t.spans.size
     assert t.valid?
     assert t.has_error?
