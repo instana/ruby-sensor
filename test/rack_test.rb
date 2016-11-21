5696ca88e09c815820301962fc2aee680e2a5fb9
Peter Giacomo Lombardo <pglombardo@gmail.com>
2016-11-21 13:18:28 +0100

    Test to validate pickup of incoming context via HTTP headers.

--- test/rack_test.rb
+++ test/rack_test.rb
@@ -36,6 +36,7 @@ class RackTest < Minitest::Test
     assert first_span[:f].key?(:e)
     assert first_span[:f].key?(:h)
     assert_equal ::Instana.agent.agent_uuid, first_span[:f][:h]
+    ::Instana.processor.clear!
   end
 
   def test_basic_post
@@ -58,6 +59,7 @@ class RackTest < Minitest::Test
     assert first_span[:f].key?(:e)
     assert first_span[:f].key?(:h)
     assert_equal ::Instana.agent.agent_uuid, first_span[:f][:h]
+    ::Instana.processor.clear!
   end
 
   def test_basic_put
@@ -80,5 +82,37 @@ class RackTest < Minitest::Test
     assert first_span[:f].key?(:e)
     assert first_span[:f].key?(:h)
     assert_equal ::Instana.agent.agent_uuid, first_span[:f][:h]
+    ::Instana.processor.clear!
   end
+
+  def test_get_with_context
+    header 'X-INSTANA-T', "1234"
+    header 'X-INSTANA-S', "4321"
+
+    get '/mrlobster'
+    assert last_response.ok?
+
+    spans = ::Instana.processor.queued_spans
+
+    # Span validation
+    assert_equal 1, spans.count
+    first_span = spans.first
+    assert_equal :rack, first_span[:n]
+    assert_equal :ruby, first_span[:ta]
+    assert first_span.key?(:data)
+    assert first_span[:data].key?(:http)
+    assert_equal "GET", first_span[:data][:http][:method]
+    assert_equal "/mrlobster", first_span[:data][:http][:url]
+    assert_equal 200, first_span[:data][:http][:status]
+    assert first_span.key?(:f)
+    assert first_span[:f].key?(:e)
+    assert first_span[:f].key?(:h)
+    assert_equal ::Instana.agent.agent_uuid, first_span[:f][:h]
+
+    # Context validation
+    assert_equal "1234", first_span[:t]
+    assert_equal "4321", first_span[:p]
+    ::Instana.processor.clear!
+  end
+
 end
