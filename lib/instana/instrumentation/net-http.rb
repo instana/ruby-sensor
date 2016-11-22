eb0227843ac82831c67a6b8319e5e4e949dacd53
Peter Giacomo Lombardo <pglombardo@gmail.com>
2016-11-22 16:25:24 +0100

    Fix span name; better debug and report http payload

--- lib/instana/instrumentation/net-http.rb
+++ lib/instana/instrumentation/net-http.rb
@@ -7,7 +7,7 @@ Net::HTTP.class_eval {
       return request_without_instana(*args, &block)
     end
 
-    ::Instana.tracer.log_entry(:net_http)
+    ::Instana.tracer.log_entry(:'net-http')
 
     # Send out the tracing context with the request
     request = args[0]
@@ -21,19 +21,24 @@ Net::HTTP.class_eval {
     response = request_without_instana(*args, &block)
 
     # Pickup response headers
-    their_trace_id = response.get_fields('X-Instana-T')
-    their_span_id = response.get_fields('X-Instana-S')
+    their_trace_id = response.get_fields('X-Instana-T').first.to_i
+    their_span_id = response.get_fields('X-Instana-S').first
 
     if their_trace_id && our_trace_id != their_trace_id
-      ::Instana.logger.debug "Trace ID mismatch on net/http response! ours: #{our_trace_id} theirs: #{their_trace_id}"
+      ::Instana.logger.debug "#{Thread.current}: Trace ID mismatch on net/http response! ours: #{our_trace_id} theirs: #{their_trace_id}"
     end
+
+    kv_payload = { :http => {} }
+    kv_payload[:http][:status] = response.code
+    kv_payload[:http][:url] = request.uri.to_s
+    ::Instana.tracer.log_info(kv_payload)
 
     response
   rescue => e
     ::Instana.tracer.log_error(e)
     raise
   ensure
-    ::Instana.tracer.log_exit(:net_http)
+    ::Instana.tracer.log_exit(:'net-http')
   end
 
   Instana.logger.info "Instrumenting net/http"
