5b996628630e41283b4e3aee59bfbdb82cb1f553
Peter Giacomo Lombardo <pglombardo@gmail.com>
2016-11-28 11:13:16 +0100

    When in Rails, auto insert ::Instana::Rack into middleware

---
+++ lib/instana/frameworks/rails.rb
@@ -0,0 +1,22 @@
+require "instana/rack"
+
+if defined?(::Rails)
+  # In Rails, let's use the Rails logger
+  ::Instana.logger = ::Rails.logger if ::Rails.logger
+
+  if ::Rails::VERSION::MAJOR < 3
+    ::Rails.configuration.after_initialization do
+      ::Instana.logger.warn "Instana: Instrumenting Rack"
+      ::Rails.configuration.middleware.insert 0, ::Instana::Rack
+    end
+  else
+    module ::Instana
+      class Railtie < ::Rails::Railtie
+        initializer 'instana.rack' do |app|
+          ::Instana.logger.warn "Instana: Instrumenting Rack"
+          app.config.middleware.insert 0, ::Instana::Rack
+        end
+      end
+    end
+  end
+end
--- lib/instana/instrumentation.rb
+++ lib/instana/instrumentation.rb
@@ -10,3 +10,16 @@ Dir.glob(pattern) do |f|
     Instana.logger.debug e.backtrace.join("\r\n")
   end
 end
+
+#
+# Load all of the files in the frameworks subdirectory
+#
+pattern = File.join(File.dirname(__FILE__), 'frameworks', '*.rb')
+Dir.glob(pattern) do |f|
+  begin
+    require f
+  rescue => e
+    Instana.logger.error "#{__method__}:#{File.basename(__FILE__)}:#{__LINE__}: #{e.message}"
+    Instana.logger.debug e.backtrace.join("\r\n")
+  end
+end
