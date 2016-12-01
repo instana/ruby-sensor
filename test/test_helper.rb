91e9f2191d45e61a10bb20f982d473f3b4c747a7
Peter Giacomo Lombardo <pglombardo@gmail.com>
2016-12-01 13:22:13 +0100

    Test Sinatra app & test suite for sinatra instrumentation

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,16 +1,16 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 ENV['INSTANA_GEM_TEST'] = 'true'
-require 'instana'
+require "rubygems"
+require "bundler/setup"
+Bundler.require(:default, :test)
 
-require 'rubygems'
-require 'bundler/setup'
 require "minitest/spec"
 require "minitest/autorun"
 require "minitest/reporters"
 require "minitest/debugger" if ENV['DEBUG']
 require 'webmock/minitest'
 
-# Boot a background thread Rack app that we can throw requests at
+# Boot background webservers to test against.
 require "./test/servers/rackapp_6511"
 
 Minitest::Reporters.use! MiniTest::Reporters::SpecReporter.new
