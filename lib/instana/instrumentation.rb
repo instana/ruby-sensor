a633066314955db339f9537b08dbc8d05978c180
Peter Giacomo Lombardo <pglombardo@gmail.com>
2016-12-02 10:23:38 +0100

    Reduce code duplication

--- lib/instana/instrumentation.rb
+++ lib/instana/instrumentation.rb
@@ -1,25 +1,18 @@
-#
-# Load all of the files in the instrumentation subdirectory
-#
-pattern = File.join(File.dirname(__FILE__), 'instrumentation', '*.rb')
-Dir.glob(pattern) do |f|
-  begin
-    require f
-  rescue => e
-    Instana.logger.error "#{__method__}:#{File.basename(__FILE__)}:#{__LINE__}: #{e.message}"
-    Instana.logger.debug e.backtrace.join("\r\n")
-  end
+module Instana
+  AUTOLOAD_DIRECTORIES = [:instrumentation, :frameworks].freeze
 end
 
 #
-# Load all of the files in the frameworks subdirectory
+# Load all of the files in the specified subdirectories
 #
-pattern = File.join(File.dirname(__FILE__), 'frameworks', '*.rb')
-Dir.glob(pattern) do |f|
-  begin
-    require f
-  rescue => e
-    Instana.logger.error "#{__method__}:#{File.basename(__FILE__)}:#{__LINE__}: #{e.message}"
-    Instana.logger.debug e.backtrace.join("\r\n")
+::Instana::AUTOLOAD_DIRECTORIES.each do |d|
+  pattern = File.join(File.dirname(__FILE__), d.to_s, '*.rb')
+  Dir.glob(pattern) do |f|
+    begin
+      require f
+    rescue => e
+      Instana.logger.error "#{__method__}:#{File.basename(__FILE__)}:#{__LINE__}: #{e.message}"
+      Instana.logger.debug e.backtrace.join("\r\n")
+    end
   end
 end
