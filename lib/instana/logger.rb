aded56431e86af7678080dd3795435bc1bb87043
Peter Giacomo Lombardo <pglombardo@gmail.com>
2016-12-07 19:33:00 +0100

    Add new trace debug level.

--- lib/instana/logger.rb
+++ lib/instana/logger.rb
@@ -2,7 +2,7 @@ require "logger"
 
 module Instana
   class XLogger < Logger
-    LEVELS = [:agent, :agent_comm].freeze
+    LEVELS = [:agent, :agent_comm, :trace].freeze
     STAMP = "Instana: ".freeze
 
     def initialize(*args)
@@ -31,6 +31,11 @@ module Instana
 
     def agent_comm(msg)
       return unless @level_agent_comm
+      self.debug(msg)
+    end
+
+    def trace(msg)
+      return unless @level_trace
       self.debug(msg)
     end
 
