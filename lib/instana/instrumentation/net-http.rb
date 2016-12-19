5f6d6c6b0e655425ec7b0c308dd65bc18f56bd2c
Peter Giacomo Lombardo <pglombardo@gmail.com>
2016-12-19 14:35:40 +0100

    Simply by using available helper methods.

--- lib/instana/instrumentation/net-http.rb
+++ lib/instana/instrumentation/net-http.rb
@@ -11,20 +11,18 @@ Net::HTTP.class_eval {
 
     # Send out the tracing context with the request
     request = args[0]
-    our_trace_id = ::Instana.tracer.trace_id
-    our_span_id  = ::Instana.tracer.span_id
 
     # Set request headers; encode IDs as hexadecimal strings
-    request['X-Instana-T'] = ::Instana.tracer.id_to_header(our_trace_id)
-    request['X-Instana-S'] = ::Instana.tracer.id_to_header(our_span_id)
+    request['X-Instana-T'] = ::Instana.tracer.trace_id_header
+    request['X-Instana-S'] = ::Instana.tracer.span_id_header
 
     response = request_without_instana(*args, &block)
 
     # Pickup response headers; convert back to base 10 integer
     if ::Instana.debug? && response.key?('X-Instana-T')
       their_trace_id = ::Instana.tracer.header_to_id(response.header['X-Instana-T'])
-      if our_trace_id != their_trace_id
-        ::Instana.logger.debug "#{Thread.current}: Trace ID mismatch on net/http response! ours: #{our_trace_id} theirs: #{their_trace_id}"
+      if ::Instana.tracer.trace_id != their_trace_id
+        ::Instana.logger.debug "#{Thread.current}: Trace ID mismatch on net/http response! ours: #{::Instana.tracer.trace_id} theirs: #{their_trace_id}"
       end
     end
 
