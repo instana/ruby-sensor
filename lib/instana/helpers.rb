ab31a49f8c398a9bf2bf49698e8ff64b4fff7b93
Peter Giacomo Lombardo <pglombardo@gmail.com>
2016-12-22 14:54:41 +0100

    Add a test snippet for development.

--- lib/instana/helpers.rb
+++ lib/instana/helpers.rb
@@ -1,6 +1,7 @@
 module Instana
   module Helpers
     EUM_SNIPPET= (File.read(File.dirname(__FILE__) + '/eum/eum.js.erb')).freeze
+    EUM_TEST_SNIPPET= (File.read(File.dirname(__FILE__) + '/eum/eum-test.js.erb')).freeze
 
     class << self
 
@@ -13,6 +14,22 @@ module Instana
         ::Instana.config[:eum_baggage] = kvs
 
         ERB.new(EUM_SNIPPET).result
+      rescue => e
+        Instana.logger.error "#{__method__}:#{File.basename(__FILE__)}:#{__LINE__}: #{e.message}"
+        Instana.logger.debug e.backtrace.join("\r\n")
+        return nil
+      end
+
+      # Returns a processed javascript snippet to be placed within the HEAD tag of an HTML page.
+      # This one is used for testing only
+      #
+      def eum_test_snippet(api_key, kvs = {})
+        return nil if !::Instana.tracer.tracing?
+
+        ::Instana.config[:eum_api_key] = api_key
+        ::Instana.config[:eum_baggage] = kvs
+
+        ERB.new(EUM_TEST_SNIPPET).result
       rescue => e
         Instana.logger.error "#{__method__}:#{File.basename(__FILE__)}:#{__LINE__}: #{e.message}"
         Instana.logger.debug e.backtrace.join("\r\n")
