d67e219e4c76a8aaa8475618a3ae1bf919f8e331
Peter Giacomo Lombardo <pglombardo@gmail.com>
2017-01-06 12:21:49 +0100

    Add tests using OT APIs against Rack server

--- test/tracing/opentracing_test.rb
+++ test/tracing/opentracing_test.rb
@@ -1,6 +1,121 @@
 require 'test_helper'
+require 'rack/test'
+require 'rack/lobster'
+require "opentracing"
+
+module Instana
+  class OTRack1
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      otrack1_span = OpenTracing.start_span(:otrack1)
+      result = @app.call(env)
+      otrack1_span.finish
+      result
+    end
+  end
+
+  class OTRack2
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      otrack2_span = OpenTracing.start_span(:otrack2)
+      result = @app.call(env)
+      otrack2_span.finish
+      result
+    end
+  end
+end
+
+OpenTracing.global_tracer = ::Instana.tracer
 
 class OpenTracerTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    @app = Rack::Builder.new {
+      use Instana::Rack
+      use Instana::OTRack1
+      use Rack::CommonLogger
+      use Rack::ShowExceptions
+      use Instana::OTRack2
+      map "/mrlobster" do
+        run Rack::Lobster.new
+      end
+    }
+  end
+
+  def test_basic_get_with_opentracing
+    ::Instana.processor.clear!
+    get '/mrlobster'
+    assert last_response.ok?
+
+    traces = ::Instana.processor.queued_traces
+    assert_equal 1, traces.count
+
+    trace = traces[0]
+    assert trace.valid?
+
+    # Span validation
+    assert_equal 3, trace.spans.count
+    first_span, second_span, third_span = trace.spans.to_a
+
+    assert_equal :rack, first_span.name
+    assert first_span.key?(:data)
+    assert first_span[:data].key?(:http)
+    assert_equal "GET", first_span[:data][:http][:method]
+    assert_equal "/mrlobster", first_span[:data][:http][:url]
+    assert_equal 200, first_span[:data][:http][:status]
+    assert_equal 'example.org', first_span[:data][:http][:host]
+    assert_equal :otrack1, second_span.name
+    assert second_span.key?(:data)
+    assert second_span[:data].key?(:sdk)
+    assert second_span[:data][:sdk].key?(:name)
+    assert_equal :otrack2, third_span.name
+    assert third_span.key?(:data)
+    assert third_span[:data].key?(:sdk)
+    assert third_span[:data][:sdk].key?(:name)
+
+    # ID Validation
+    assert_equal trace.id, first_span[:t]
+    assert_equal trace.id, third_span[:t]
+
+    # Linkage
+    assert_equal second_span[:s], third_span[:p]
+  end
+
+  def test_get_with_inject_extract
+    ::Instana.processor.clear!
+
+    trace_id = ::Instana::Util.generate_id
+    span_id = ::Instana::Util.generate_id
+
+    header 'X-Instana-T', ::Instana::Util.id_to_header(trace_id)
+    header 'X-Instana-S', ::Instana::Util.id_to_header(span_id)
+
+    get '/mrlobster'
+    assert last_response.ok?
+
+    traces = ::Instana.processor.queued_traces
+    assert_equal 1, traces.count
+
+    trace = traces[0]
+    assert trace.valid?
+
+    # Span validation
+    assert_equal 3, trace.spans.count
+    first_span = trace.spans.to_a.first
+
+    # Make sure context was picked up and continued in the resulting
+    # trace
+    assert_equal trace_id, trace.id
+    assert_equal span_id, first_span[:p]
+  end
+
   def test_start_span_with_tags
     ::Instana.processor.clear!
     otracer = ::Instana.tracer
@@ -60,7 +175,7 @@ class OpenTracerTest < Minitest::Test
     otracer = ::Instana.tracer
     entry_span = otracer.start_span(:rack)
     ac_span = otracer.start_span(:action_controller, child_of: entry_span)
-    av_span = otracer.start_span(:action_view, child_of: entry_span)
+    av_span = otracer.start_span(:action_view, child_of: ac_span)
     sleep 0.1
     av_span.finish
     ac_span.finish
@@ -69,11 +184,12 @@ class OpenTracerTest < Minitest::Test
     traces = ::Instana.processor.queued_traces
 
     assert_equal 1, traces.count
-    trace = traces.first
-    assert trace.valid?
-    assert_equal 3, trace.spans.count
 
+    trace = traces.first
     first_span, second_span, third_span = trace.spans.to_a
+
+    assert_equal 3, trace.spans.count
+    assert trace.valid?
 
     # IDs
     assert_equal trace.id, first_span[:t]
@@ -83,7 +199,7 @@ class OpenTracerTest < Minitest::Test
     # Linkage
     assert first_span.is_root?
     assert_equal first_span[:s], second_span[:p]
-    assert_equal first_span[:s], third_span[:p]
+    assert_equal second_span[:s], third_span[:p]
   end
 
   def test_start_span_with_nested_spans
