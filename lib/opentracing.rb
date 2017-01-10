803951a6130c838647eced8c77879e4a829e5bf3
Peter Giacomo Lombardo <pglombardo@gmail.com>
2017-01-10 13:15:37 +0100

    Set the OT tracer to our own OT compatible tracer.

--- lib/opentracing.rb
+++ lib/opentracing.rb
@@ -1,2 +1,6 @@
 require "instana/opentracing/tracer"
 require "instana/opentracing/carrier"
+
+# Set the global tracer to our OT tracer
+# which supports the OT specificiation
+OpenTracing.global_tracer = ::Instana.tracer
