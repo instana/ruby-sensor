81d773cd7c481e81d5d17fa88f9f4ea894bd0920
Peter Giacomo Lombardo <pglombardo@gmail.com>
2017-01-16 17:24:29 +0100

    Add get_multi support
    - determine actual server the request went to
    - add tests for the above

--- lib/instana/instrumentation/dalli.rb
+++ lib/instana/instrumentation/dalli.rb
@@ -7,15 +7,62 @@ module Instana
       end
 
       def perform_with_instana(*args, &blk)
-        ::Instana.tracer.log_entry(:memcache, { :memcache => { :command => args[0] } })
-        perform_without_instana(*args, &blk)
+        if !::Instana.tracer.tracing? || ::Instana.tracer.tracing_span?(:memcache)
+          return perform_without_instana(*args, &blk)
+        end
+
+        op, key, *_opts = args
+
+        entry_payload = { :memcache => {} }
+        entry_payload[:memcache][:namespace] = @options[:namespace] if @options.key?(:namespace)
+        entry_payload[:memcache][:command] = op
+        entry_payload[:memcache][:key] = key
+
+        ::Instana.tracer.log_entry(:memcache, entry_payload)
+        result = perform_without_instana(*args, &blk)
+
+        kv_payload = { :memcache => {}}
+        kv_payload[:memcache][:hit] = result ? true : false
+        result
       rescue => e
         ::Instana.tracer.log_error(e)
+        raise
       ensure
-        ::Instana.tracer.log_exit(:memcache)
+        ::Instana.tracer.log_exit(:memcache, kv_payload)
       end
 
       def get_multi_with_instana(*keys)
+        entry_payload = { :memcache => {} }
+        entry_payload[:memcache][:namespace] = @options[:namespace] if @options.key?(:namespace)
+        entry_payload[:memcache][:command] = :get_multi
+        entry_payload[:memcache][:keys] = keys.flatten
+
+        ::Instana.tracer.log_entry(:memcache, entry_payload)
+        result = get_multi_without_instana(*keys)
+
+        kv_payload = {}
+        kv_payload[:hit_count] = result.length
+        result
+      rescue => e
+        ::Instana.tracer.log_error(e)
+        raise
+      ensure
+        ::Instana.tracer.log_exit(:memcache, kv_payload)
+      end
+    end
+
+    module DalliServer
+      def self.included(klass)
+        ::Instana::Util.method_alias(klass, :request)
+      end
+
+      def request_with_instana(op, *args)
+        if ::Instana.tracer.tracing? || ::Instana.tracer.tracing_span?(:memcache)
+          info_payload = { :memcache => {} }
+          info_payload[:memcache][:server] = "#{@hostname}:#{@port}"
+          ::Instana.tracer.log_info(info_payload)
+        end
+        request_without_instana(op, *args)
       end
     end
   end
@@ -24,4 +71,5 @@ end
 if defined?(::Dalli) && ::Instana.config[:dalli][:enabled]
   ::Instana.logger.warn "Instrumenting Dalli"
   ::Dalli::Client.send(:include, ::Instana::Instrumentation::Dalli)
+  ::Dalli::Server.send(:include, ::Instana::Instrumentation::DalliServer)
 end
--- test/instrumentation/dalli_test.rb
+++ test/instrumentation/dalli_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class DalliTest < Minitest::Test
   def setup
     @memcached_host = ENV['MEMCACHED_HOST'] || '127.0.0.1:11211'
-    @dc = Dalli::Client.new(@memcached_host)
+    @dc = Dalli::Client.new(@memcached_host, :namespace => "instana_test")
   end
 
   def test_config_defaults
@@ -15,9 +15,14 @@ class DalliTest < Minitest::Test
   def test_basic_get
     clear_all!
 
+    @dc.set(:instana, :boom)
+
+    result = nil
     ::Instana.tracer.start_or_continue_trace(:dalli_test) do
-      @dc.get(:instana)
+      result = @dc.get(:instana)
     end
+
+    assert_equal :boom, result
 
     traces = Instana.processor.queued_traces
     assert_equal 1, traces.count
@@ -37,14 +42,23 @@ class DalliTest < Minitest::Test
     assert second_span[:data].key?(:memcache)
     assert second_span[:data][:memcache].key?(:command)
     assert_equal :get, second_span[:data][:memcache][:command]
+    assert second_span[:data][:memcache].key?(:key)
+    assert_equal :instana, second_span[:data][:memcache][:key]
+    assert second_span[:data][:memcache].key?(:namespace)
+    assert_equal 'instana_test', second_span[:data][:memcache][:namespace]
+    assert second_span[:data][:memcache].key?(:server)
+    assert_equal "192.168.201.128:11211", second_span[:data][:memcache][:server]
   end
 
   def test_basic_set
     clear_all!
 
+    result = nil
     ::Instana.tracer.start_or_continue_trace(:dalli_test) do
-      @dc.set(:instana, :rocks)
+      result = @dc.set(:instana, :rocks)
     end
+
+    assert result.is_a?(Integer)
 
     traces = Instana.processor.queued_traces
     assert_equal 1, traces.count
@@ -64,5 +78,118 @@ class DalliTest < Minitest::Test
     assert second_span[:data].key?(:memcache)
     assert second_span[:data][:memcache].key?(:command)
     assert_equal :set, second_span[:data][:memcache][:command]
+    assert second_span[:data][:memcache].key?(:key)
+    assert_equal :instana, second_span[:data][:memcache][:key]
+    assert second_span[:data][:memcache].key?(:namespace)
+    assert_equal 'instana_test', second_span[:data][:memcache][:namespace]
+    assert second_span[:data][:memcache].key?(:server)
+    assert_equal "192.168.201.128:11211", second_span[:data][:memcache][:server]
+  end
+
+  def test_incr
+    clear_all!
+
+    result = nil
+    @dc.set(:counter, 0, nil, :raw => true)
+    ::Instana.tracer.start_or_continue_trace(:dalli_test) do
+      result = @dc.incr(:counter, 1, nil, 0)
+    end
+
+    assert_equal 1, result
+
+    traces = Instana.processor.queued_traces
+    assert_equal 1, traces.count
+    trace = traces.first
+
+    # Excon validation
+    assert_equal 2, trace.spans.count
+    spans = trace.spans.to_a
+    first_span = spans[0]
+    second_span = spans[1]
+
+    assert_equal :dalli_test, first_span.name
+    assert_equal :memcache, second_span.name
+    assert_equal false, second_span.key?(:error)
+    assert second_span[:p] == first_span[:s]
+    assert first_span[:t] == first_span[:s]
+    assert second_span[:data].key?(:memcache)
+    assert second_span[:data][:memcache].key?(:command)
+    assert_equal :incr, second_span[:data][:memcache][:command]
+    assert second_span[:data][:memcache].key?(:key)
+    assert_equal :counter, second_span[:data][:memcache][:key]
+    assert second_span[:data][:memcache].key?(:namespace)
+    assert_equal 'instana_test', second_span[:data][:memcache][:namespace]
+    assert second_span[:data][:memcache].key?(:server)
+    assert_equal "192.168.201.128:11211", second_span[:data][:memcache][:server]
+  end
+
+  def test_decr
+    clear_all!
+
+    result = nil
+    @dc.set(:counter, 0, nil, :raw => true)
+    ::Instana.tracer.start_or_continue_trace(:dalli_test) do
+      result = @dc.decr(:counter, 1, nil, 0)
+    end
+
+    assert_equal 0, result
+
+    traces = Instana.processor.queued_traces
+    assert_equal 1, traces.count
+    trace = traces.first
+
+    # Excon validation
+    assert_equal 2, trace.spans.count
+    spans = trace.spans.to_a
+    first_span = spans[0]
+    second_span = spans[1]
+
+    assert_equal :dalli_test, first_span.name
+    assert_equal :memcache, second_span.name
+    assert_equal false, second_span.key?(:error)
+    assert second_span[:p] == first_span[:s]
+    assert first_span[:t] == first_span[:s]
+    assert second_span[:data].key?(:memcache)
+    assert second_span[:data][:memcache].key?(:command)
+    assert_equal :decr, second_span[:data][:memcache][:command]
+    assert second_span[:data][:memcache].key?(:key)
+    assert_equal :counter, second_span[:data][:memcache][:key]
+    assert second_span[:data][:memcache].key?(:namespace)
+    assert_equal 'instana_test', second_span[:data][:memcache][:namespace]
+    assert second_span[:data][:memcache].key?(:server)
+    assert_equal "192.168.201.128:11211", second_span[:data][:memcache][:server]
+  end
+
+  def test_get_multi
+    clear_all!
+
+    ::Instana.tracer.start_or_continue_trace(:dalli_test) do
+      @dc.get_multi(:one, :two, :three, :four)
+    end
+
+    traces = Instana.processor.queued_traces
+    assert_equal 1, traces.count
+    trace = traces.first
+
+    # Excon validation
+    assert_equal 2, trace.spans.count
+    spans = trace.spans.to_a
+    first_span = spans[0]
+    second_span = spans[1]
+
+    assert_equal :dalli_test, first_span.name
+    assert_equal :memcache, second_span.name
+    assert_equal false, second_span.key?(:error)
+    assert second_span[:p] == first_span[:s]
+    assert first_span[:t] == first_span[:s]
+    assert second_span[:data].key?(:memcache)
+    assert second_span[:data][:memcache].key?(:command)
+    assert_equal :get_multi, second_span[:data][:memcache][:command]
+    assert second_span[:data][:memcache].key?(:keys)
+    assert_equal [:one, :two, :three, :four], second_span[:data][:memcache][:keys]
+    assert second_span[:data][:memcache].key?(:namespace)
+    assert_equal 'instana_test', second_span[:data][:memcache][:namespace]
+    assert second_span[:data][:memcache].key?(:server)
+    assert_equal "192.168.201.128:11211", second_span[:data][:memcache][:server]
   end
 end
