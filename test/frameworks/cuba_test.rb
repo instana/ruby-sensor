4fe20d42e98d9a31d70a54b793334cb1a7de4127
Peter Giacomo Lombardo <pglombardo@gmail.com>
2017-01-18 14:58:58 +0100

    Make sure frameworks are defined before testing.

--- test/frameworks/cuba_test.rb
+++ test/frameworks/cuba_test.rb
@@ -1,41 +1,44 @@
-require 'test_helper'
-require File.expand_path(File.dirname(__FILE__) + '/../apps/cuba')
-require 'rack/test'
 
-class CubaTest < Minitest::Test
-  include Rack::Test::Methods
+if defined?(::Cuba)
+  require 'test_helper'
+  require File.expand_path(File.dirname(__FILE__) + '/../apps/cuba')
+  require 'rack/test'
 
-  def app
-    Cuba
-  end
+  class CubaTest < Minitest::Test
+    include Rack::Test::Methods
 
-  def test_basic_get
-    clear_all!
+    def app
+      Cuba
+    end
 
-    r = get '/hello'
-    assert last_response.ok?
+    def test_basic_get
+      clear_all!
 
-    assert r.headers.key?("X-Instana-T")
-    assert r.headers.key?("X-Instana-S")
+      r = get '/hello'
+      assert last_response.ok?
 
-    spans = ::Instana.processor.queued_spans
-    assert_equal 1, spans.count
+      assert r.headers.key?("X-Instana-T")
+      assert r.headers.key?("X-Instana-S")
 
-    first_span = spans.first
-    assert_equal :rack, first_span[:n]
-    assert first_span.key?(:data)
-    assert first_span[:data].key?(:http)
+      spans = ::Instana.processor.queued_spans
+      assert_equal 1, spans.count
 
-    assert first_span[:data][:http].key?(:method)
-    assert_equal "GET", first_span[:data][:http][:method]
+      first_span = spans.first
+      assert_equal :rack, first_span[:n]
+      assert first_span.key?(:data)
+      assert first_span[:data].key?(:http)
 
-    assert first_span[:data][:http].key?(:url)
-    assert_equal "/hello", first_span[:data][:http][:url]
+      assert first_span[:data][:http].key?(:method)
+      assert_equal "GET", first_span[:data][:http][:method]
 
-    assert first_span[:data][:http].key?(:status)
-    assert_equal 200, first_span[:data][:http][:status]
+      assert first_span[:data][:http].key?(:url)
+      assert_equal "/hello", first_span[:data][:http][:url]
 
-    assert first_span[:data][:http].key?(:host)
-    assert_equal "example.org", first_span[:data][:http][:host]
+      assert first_span[:data][:http].key?(:status)
+      assert_equal 200, first_span[:data][:http][:status]
+
+      assert first_span[:data][:http].key?(:host)
+      assert_equal "example.org", first_span[:data][:http][:host]
+    end
   end
 end
