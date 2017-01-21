915266e03cc81d8d69b2dc1608a709282da96347
Peter Giacomo Lombardo <pglombardo@gmail.com>
2017-01-21 22:57:39 +0100

    Add ActionPack 5 instrumentation support via prepend

--- lib/instana/frameworks/instrumentation/action_controller.rb
+++ lib/instana/frameworks/instrumentation/action_controller.rb
@@ -7,15 +7,38 @@ module Instana
         end
       end
 
+      # The Instana wrapper method for ActionController::Base.process_action
+      # for versions 3 and 4.
+      #
       def process_action_with_instana(*args)
         kv_payload = { :actioncontroller => {} }
         kv_payload[:actioncontroller][:controller] = self.class.name
         kv_payload[:actioncontroller][:action] = action_name
 
         ::Instana.tracer.log_entry(:actioncontroller, kv_payload)
+
         process_action_without_instana(*args)
       rescue => e
         ::Instana.tracer.log_error(e)
+        raise
+      ensure
+        ::Instana.tracer.log_exit(:actioncontroller)
+      end
+
+      # This is the Rails 5 version of the method above where we use prepend to
+      # instrument the method instead of using alias_method_chain.
+      #
+      def process_action(*args)
+        kv_payload = { :actioncontroller => {} }
+        kv_payload[:actioncontroller][:controller] = self.class.name
+        kv_payload[:actioncontroller][:action] = action_name
+
+        ::Instana.tracer.log_entry(:actioncontroller, kv_payload)
+
+        super(*args)
+      rescue => e
+        ::Instana.tracer.log_error(e)
+        raise
       ensure
         ::Instana.tracer.log_exit(:actioncontroller)
       end
