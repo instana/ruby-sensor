de42fcf60fa3fe9340337466213de21e5a9bf307
Nguyễn Quang Minh <nguyenquangminh0711@gmail.com>
2017-06-06 18:56:37 +0700

    Standardize span content and scope down GRPC unary request only

--- lib/instana/instrumentation/grpc.rb
+++ lib/instana/instrumentation/grpc.rb
@@ -5,11 +5,13 @@ if defined?(GRPC::ActiveCall)
         return request_response_without_instana(method, *others, **options)
       end
 
-      ::Instana.tracer.log_entry(:'net-http')
-      kv_payload = { http: {} }
-      kv_payload[:http][:method] = 'GRPC'
-      kv_payload[:http][:host] = @host
-      kv_payload[:http][:url] = method
+      parts = method.split('/')
+      kvs = {
+        host: @host,
+        service: parts[1],
+        rpc: parts[2]
+      }
+      ::Instana.tracer.log_entry(:'grpc-call', kvs)
 
       context = ::Instana.tracer.context
       if context
@@ -24,7 +26,7 @@ if defined?(GRPC::ActiveCall)
       ::Instana.tracer.log_error(e)
       raise
     ensure
-      ::Instana.tracer.log_exit(:'net-http', kv_payload)
+      ::Instana.tracer.log_exit(:'grpc-call', {})
     end
 
     ::Instana.logger.warn 'Instrumenting GRPC client'
@@ -35,34 +37,41 @@ if defined?(GRPC::ActiveCall)
 end
 
 if defined?(GRPC::RpcDesc)
-  GRPC::RpcDesc.class_eval do
-    def run_server_method_with_instana(active_call, mth)
-      metadata = active_call.metadata
+  [:handle_request_response, :handle_client_streamer].each do |method|
+    GRPC::RpcDesc.class_eval(
+      <<-RUBY, __FILE__, __LINE__ + 1
+        def #{method}_with_instana(active_call, mth)
+          metadata = active_call.metadata
 
-      kvs = { http: {} }
-      kvs[:http][:method] = 'GRPC'
-      kvs[:http][:url] = "/#{mth.owner.service_name}/#{name}"
-      kvs[:http][:host] = Socket.gethostname
+          incoming_context = {}
+          if metadata.key?('x-instana-t')
+            incoming_context[:trace_id]  = ::Instana::Util.header_to_id(metadata['x-instana-t'])
+            incoming_context[:span_id]   = ::Instana::Util.header_to_id(metadata['x-instana-s']) if metadata.key?('x-instana-s')
+            incoming_context[:level]     = metadata['x-instana-l'] if metadata.key?('x-instana-l')
+          end
 
-      incoming_context = {}
-      if metadata.key?('x-instana-t')
-        incoming_context[:trace_id]  = ::Instana::Util.header_to_id(metadata['x-instana-t'])
-        incoming_context[:span_id]   = ::Instana::Util.header_to_id(metadata['x-instana-s']) if metadata.key?('x-instana-s')
-        incoming_context[:level]     = metadata['x-instana-l'] if metadata.key?('x-instana-l')
-      end
+          kvs = {
+            host: Socket.gethostname,
+            service: mth.owner.service_name,
+            rpc: name
+          }
+          ::Instana.tracer.log_start_or_continue(
+            :'grpc-server', kvs, incoming_context
+          )
 
-      ::Instana.tracer.log_start_or_continue(:rack, {}, incoming_context)
-      run_server_method_without_instana(active_call, mth)
-    rescue => e
-      ::Instana.tracer.log_error(e)
-      raise
-    ensure
-      ::Instana.tracer.log_end(:rack, kvs)
-    end
+          #{method}_without_instana(active_call, mth)
+        rescue => e
+          ::Instana.tracer.log_error(e)
+          raise
+        ensure
+          ::Instana.tracer.log_end(:'grpc-server', {})
+        end
 
-    ::Instana.logger.warn 'Instrumenting GRPC server'
+        ::Instana.logger.warn 'Instrumenting GRPC server'
 
-    alias run_server_method_without_instana run_server_method
-    alias run_server_method run_server_method_with_instana
+        alias #{method}_without_instana #{method}
+        alias #{method} #{method}_with_instana
+      RUBY
+    )
   end
 end
