08f7baf381e5c5cae0cc9be3e955947b3614ae7f
Peter Giacomo Lombardo <pglombardo@gmail.com>
2017-08-01 16:03:46 +0200

    Add sidekiq development helper.

---
+++ test/servers/helpers/sidekiq_worker_initializer.rb
@@ -0,0 +1,22 @@
+# This is a helper initializer to boot a local instrumented Sidekiq stack
+# for development and testing.
+#
+# export BUNDLE_GEMFILE=/path/to/ruby-sensor/gemfiles/libraries.gemfile
+# bundle install
+# bundle exec sidekiq -c 2 -q important -r ./test/jobs/sidekiq_job_1.rb -r ./test/jobs/sidekiq_job_2.rb
+#
+# In another shell, you can boot a console to queue jobs:
+#
+# export BUNDLE_GEMFILE=/path/to/ruby-sensor/gemfiles/libraries.gemfile
+# bundle install
+# bundle exec rake console
+# > Instana.tracer.start_or_continue_trace(:sidekiq_demo) do
+# >  ::Sidekiq::Client.push( 'queue' => 'important', 'class' => ::SidekiqJobOne,
+# >                          'args' => [1, 2, 3], 'retry' => false)
+# > end
+#
+require Dir.pwd + '/test/jobs/sidekiq_job_1.rb'
+require Dir.pwd + '/test/jobs/sidekiq_job_2.rb'
+
+::Instana.logger.info "Booting instrumented sidekiq worker for tests."
+::Sidekiq.logger.level = ::Logger::DEBUG
