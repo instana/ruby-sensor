03cdf34a1807f0c5ebfcc8bea95725edbc2315a0
Peter Giacomo Lombardo <pglombardo@gmail.com>
2017-09-12 16:46:37 +0200

    Basic opentracing start_span benchmark

---
+++ benchmarks/opentracing.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+require "bundler"
+Bundler.require(:default)
+
+require "benchmark"
+
+Benchmark.bm do |x|
+  x.report("start_span, finish:             ") {
+    50_000.times {
+      ::Instana.tracer.start_span(:blah).finish
+    }
+  }
+
+  x.report("start_span, set_tag(5x), finish:") {
+    50_000.times {
+      span = ::Instana.tracer.start_span(:blah)
+      span.set_tag(:blah, 1)
+      span.set_tag(:dog, 1)
+      span.set_tag(:moon, "ok")
+      span.set_tag(:ape, 1)
+      span.set_tag(:blah, 1)
+      span.finish
+    }
+  }
+
+end
