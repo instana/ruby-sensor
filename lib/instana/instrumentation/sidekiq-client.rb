b3e22669c9750427bd61cf4ac04b51c212e95972
Peter Giacomo Lombardo <pglombardo@gmail.com>
2018-03-21 10:52:42 +0100

    Assure 'Instrumenting' log messages are level INFO

--- lib/instana/instrumentation/sidekiq-client.rb
+++ lib/instana/instrumentation/sidekiq-client.rb
@@ -38,7 +38,7 @@ end
 if defined?(::Sidekiq) && ::Instana.config[:'sidekiq-client'][:enabled]
   ::Sidekiq.configure_client do |cfg|
     cfg.client_middleware do |chain|
-      ::Instana.logger.warn "Instrumenting Sidekiq client"
+      ::Instana.logger.info "Instrumenting Sidekiq client"
       chain.add ::Instana::Instrumentation::SidekiqClient
     end
   end
