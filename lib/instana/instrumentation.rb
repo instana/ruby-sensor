d4633e9186aec7d802914d64ab065c660cddb861
Peter Giacomo Lombardo <pglombardo@gmail.com>
2018-08-22 12:10:14 +0200

    Don't load instr if INSTANA_DISABLE_AUTO_INSTR is set.

--- lib/instana/instrumentation.rb
+++ lib/instana/instrumentation.rb
@@ -2,17 +2,20 @@ module Instana
   AUTOLOAD_DIRECTORIES = [:instrumentation, :frameworks].freeze
 end
 
-#
-# Load all of the files in the specified subdirectories
-#
-::Instana::AUTOLOAD_DIRECTORIES.each do |d|
-  pattern = File.join(File.dirname(__FILE__), d.to_s, '*.rb')
-  Dir.glob(pattern) do |f|
-    begin
-      require f
-    rescue => e
-      Instana.logger.error "#{__method__}:#{File.basename(__FILE__)}:#{__LINE__}: #{e.message}"
-      Instana.logger.debug e.backtrace.join("\r\n")
+
+if !ENV.key?('INSTANA_DISABLE_AUTO_INSTR')
+  #
+  # Load all of the files in the specified subdirectories
+  #
+  ::Instana::AUTOLOAD_DIRECTORIES.each do |d|
+    pattern = File.join(File.dirname(__FILE__), d.to_s, '*.rb')
+    Dir.glob(pattern) do |f|
+      begin
+        require f
+      rescue => e
+        Instana.logger.error "#{__method__}:#{File.basename(__FILE__)}:#{__LINE__}: #{e.message}"
+        Instana.logger.debug e.backtrace.join("\r\n")
+      end
     end
   end
 end
