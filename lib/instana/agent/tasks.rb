6348b5bf1df1e0ba20ebaa8d585b59f273c06d90
Jordan Day <jordan@roboninja.com>
2019-01-18 10:50:07 -0600

    Pass OJ options in to dump/load calls instead of setting them as defaults.

--- lib/instana/agent/tasks.rb
+++ lib/instana/agent/tasks.rb
@@ -4,8 +4,11 @@ module AgentTasks
   #
   # @param json_string [String] the requests from the host agent
   #
+
+  OJ_OPTIONS = {mode: :strict}
+
   def handle_agent_tasks(json_string)
-    tasks = Oj.load(json_string)
+    tasks = Oj.load(json_string, OJ_OPTIONS)
 
     if tasks.is_a?(Hash)
       process_agent_task(tasks)
@@ -34,7 +37,7 @@ module AgentTasks
     path = "com.instana.plugin.ruby/response.#{@process[:report_pid]}?messageId=#{URI.encode(task['messageId'])}"
     uri = URI.parse("http://#{@discovered[:agent_host]}:#{@discovered[:agent_port]}/#{path}")
     req = Net::HTTP::Post.new(uri)
-    req.body = Oj.dump(payload)
+    req.body = Oj.dump(payload, OJ_OPTIONS)
     ::Instana.logger.debug "Responding to agent request: #{req.inspect}"
     make_host_agent_request(req)
 
