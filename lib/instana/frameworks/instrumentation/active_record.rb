295b98cfb48047be227b446dd56fcb4298cc50c7
Peter Giacomo Lombardo <pglombardo@gmail.com>
2019-08-19 16:52:08 +0200

    Support case of multiple ActiveRecord adapters

--- lib/instana/frameworks/instrumentation/active_record.rb
+++ lib/instana/frameworks/instrumentation/active_record.rb
@@ -11,18 +11,17 @@ if defined?(::ActiveRecord) && ::Instana.config[:active_record][:enabled]
     ::Instana.logger.info "Instrumenting ActiveRecord (mysql)"
     ActiveRecord::ConnectionAdapters::MysqlAdapter.send(:include, ::Instana::Instrumentation::MysqlAdapter)
     ActiveRecord::ConnectionAdapters::AbstractMysqlAdapter.send(:include, ::Instana::Instrumentation::AbstractMysqlAdapter)
+  end
 
   # Mysql2
-  elsif defined?(ActiveRecord::ConnectionAdapters::Mysql2Adapter)
+  if defined?(ActiveRecord::ConnectionAdapters::Mysql2Adapter)
     ::Instana.logger.info "Instrumenting ActiveRecord (mysql2)"
     ActiveRecord::ConnectionAdapters::Mysql2Adapter.send(:include, ::Instana::Instrumentation::Mysql2Adapter)
+  end
 
   # Postgres
-  elsif defined?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+  if defined?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
     ::Instana.logger.info "Instrumenting ActiveRecord (postgresql)"
     ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.send(:include, ::Instana::Instrumentation::PostgreSQLAdapter)
-
-  else
-    ::Instana.logger.debug "Unsupported ActiveRecord adapter"
   end
 end
