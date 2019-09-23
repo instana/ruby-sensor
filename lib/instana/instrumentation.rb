5c44b2ab56365d12024b78d25be4ba54ffed6a10
Peter Giacomo Lombardo <pglombardo@gmail.com>
2019-09-23 13:27:17 +0200

    Add option to disable gem via environment variable (#164)
    * INSTANA_DISABLE to disable the gem
    
    * Show env var in log message
    
    * Add deprecation note
    
    * Don't load auto instr when disabled
    
    * info level log when disabled
    
    * All doc links leads to Instana documentation portal

--- lib/instana/instrumentation.rb
+++ lib/instana/instrumentation.rb
@@ -2,8 +2,12 @@ module Instana
   AUTOLOAD_DIRECTORIES = [:instrumentation, :frameworks].freeze
 end
 
-
-if !ENV.key?('INSTANA_DISABLE_AUTO_INSTR') || ENV['INSTANA_DISABLE_AUTO_INSTR'] === 'false'
+# Environment variables:
+#
+# INSTANA_DISABLE_AUTO_INSTR, unless set to false, will disable loading of automatic instrumentation
+# INSTANA_DISABLE disables the gem entirely and therefore doesn't load automatic instrumentation
+#
+if (!ENV.key?('INSTANA_DISABLE_AUTO_INSTR') || ENV['INSTANA_DISABLE_AUTO_INSTR'] === 'false') && !ENV.key?('INSTANA_DISABLE')
   #
   # Load all of the files in the specified subdirectories
   #
