81f43e42dc5d2d79c0185e1c2535339195bbbe19
Hunter Madison <hunter.madison@instana.com>
2021-01-06 07:53:35 -0500

    Path template support for Sinatra

--- lib/instana/frameworks/sinatra.rb
+++ lib/instana/frameworks/sinatra.rb
@@ -3,7 +3,24 @@ require "instana/rack"
 # This instrumentation will insert Rack into Sinatra _and_ Padrino since
 # the latter is based on Sinatra
 
+module Instana
+  module SinatraPathTemplateExtractor
+    def self.extended(base)
+      ::Instana.logger.debug "#{base} extended #{self}"
+      base.store_path_template
+    end
+    
+    def store_path_template
+      after do
+        @env["INSTANA_HTTP_PATH_TEMPLATE"] = @env["sinatra.route"]
+          .sub("#{@request.request_method} ", '')
+      end
+    end
+  end
+end
+
 if defined?(::Sinatra)
   ::Instana.logger.debug "Instrumenting Sinatra"
   ::Sinatra::Base.use ::Instana::Rack
+  ::Sinatra::Base.register ::Instana::SinatraPathTemplateExtractor
 end
--- test/apps/sinatra.rb
+++ test/apps/sinatra.rb
@@ -2,4 +2,8 @@ class InstanaSinatraApp < ::Sinatra::Base
   get '/' do
     "Hello Sinatra!"
   end
+  
+  get '/greet/:name' do
+    "Hello, #{params[:name]}!"
+  end
 end
