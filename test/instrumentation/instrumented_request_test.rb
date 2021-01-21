f28c04bac0ede3bcb3725bb08ad1b58d2b343e13
Hunter Madison <hunter.madison@instana.com>
2021-01-21 13:08:21 -0500

    Formatting fixes

--- test/instrumentation/instrumented_request_test.rb
+++ test/instrumentation/instrumented_request_test.rb
@@ -1,20 +1,20 @@
 require 'test_helper'
 
 class InstrumentedRequestTest < Minitest::Test
-	def test_skip_trace_with_header
+  def test_skip_trace_with_header
     req = Instana::InstrumentedRequest.new(
       'HTTP_X_INSTANA_L' => '0;sample-data'
     )
-    
-		assert req.skip_trace?
-	end
-  
+
+    assert req.skip_trace?
+  end
+
   def test_skip_trace_without_header
     req = Instana::InstrumentedRequest.new({})
-    
+
     refute req.skip_trace?
   end
-  
+
   def test_incomming_context
     id = Instana::Util.generate_id
     req = Instana::InstrumentedRequest.new(
@@ -22,25 +22,25 @@ class InstrumentedRequestTest < Minitest::Test
       'HTTP_X_INSTANA_T' => id,
       'HTTP_X_INSTANA_S' => id
     )
-    
+
     expected = {
       trace_id: id,
       span_id: id,
       level: '1'
     }
-    
+
     assert_equal expected, req.incoming_context
   end
-  
+
   def test_request_tags
-    ::Instana.agent.extra_headers = %w(X-Capture-This)
+    ::Instana.agent.extra_headers = %w[X-Capture-This]
     req = Instana::InstrumentedRequest.new(
       'HTTP_HOST' => 'example.com',
       'REQUEST_METHOD' => 'GET',
       'HTTP_X_CAPTURE_THIS' => 'that',
       'PATH_INFO' => '/'
     )
-    
+
     expected = {
       method: 'GET',
       url: '/',
@@ -49,36 +49,36 @@ class InstrumentedRequestTest < Minitest::Test
         "X-Capture-This": 'that'
       }
     }
-    
+
     assert_equal expected, req.request_tags
     ::Instana.agent.extra_headers = nil
   end
-  
+
   def test_correlation_data_valid
     req = Instana::InstrumentedRequest.new(
       'HTTP_X_INSTANA_L' => '1,correlationType=web ;correlationId=1234567890abcdef'
     )
-    expected = { 
-      type: 'web', 
-      id: '1234567890abcdef' 
+    expected = {
+      type: 'web',
+      id: '1234567890abcdef'
     }
-        
+
     assert_equal expected, req.correlation_data
   end
-  
+
   def test_correlation_data_invalid
     req = Instana::InstrumentedRequest.new(
       'HTTP_X_INSTANA_L' => '0;sample-data'
     )
-        
+
     assert_equal({}, req.correlation_data)
   end
-  
+
   def test_correlation_data_legacy
     req = Instana::InstrumentedRequest.new(
       'HTTP_X_INSTANA_L' => '1'
     )
-        
+
     assert_equal({}, req.correlation_data)
   end
 end
