c60e03e195f74d0fa0d4a7bda326276e4d47dbe4
Hunter Madison <hunter.madison@instana.com>
2021-01-25 06:38:56 -0500

    Migrate RestClient instrumentation to .prepend

--- lib/instana/instrumentation/rest-client.rb
+++ lib/instana/instrumentation/rest-client.rb
@@ -1,34 +1,24 @@
 module Instana
   module Instrumentation
     module RestClientRequest
-      def self.included(klass)
-        if klass.method_defined?(:execute)
-          klass.class_eval do
-            alias execute_without_instana execute
-            alias execute execute_with_instana
-          end
-        end
-      end
-
-      def execute_with_instana & block
+      def execute(&block)
         # Since RestClient uses net/http under the covers, we just
         # provide span visibility here.  HTTP related KVs are reported
         # in the Net::HTTP instrumentation
         ::Instana.tracer.log_entry(:'rest-client')
 
-        execute_without_instana(&block)
+        super(&block)
       rescue => e
         ::Instana.tracer.log_error(e)
         raise
       ensure
         ::Instana.tracer.log_exit(:'rest-client')
       end
-
     end
   end
 end
 
 if defined?(::RestClient::Request) && ::Instana.config[:'rest-client'][:enabled]
   ::Instana.logger.debug "Instrumenting RestClient"
-  ::RestClient::Request.send(:include, ::Instana::Instrumentation::RestClientRequest)
+  ::RestClient::Request.send(:prepend, ::Instana::Instrumentation::RestClientRequest)
 end
