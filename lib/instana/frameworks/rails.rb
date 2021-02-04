fc1e0d13199d94c9ca9b5649b18ea2d183e0fb21
Hunter Madison <hunter.madison@instana.com>
2021-02-04 08:14:15 -0500

    Clean up the Railtie

--- lib/instana/frameworks/rails.rb
+++ lib/instana/frameworks/rails.rb
@@ -1,31 +1,27 @@
-require "instana/rack"
+if ::Rails::VERSION::MAJOR < 3
+  ::Rails.configuration.after_initialize do
+    # In Rails, let's use the Rails logger
+    ::Instana.logger = ::Rails.logger if ::Rails.logger
 
-if defined?(::Rails)
-  if ::Rails::VERSION::MAJOR < 3
-    ::Rails.configuration.after_initialize do
-      # In Rails, let's use the Rails logger
-      ::Instana.logger = ::Rails.logger if ::Rails.logger
-
-      if ::Instana.config[:tracing][:enabled]
-        ::Instana.logger.debug "Instrumenting Rack"
-        ::Rails.configuration.middleware.insert 0, ::Instana::Rack
-      else
-        ::Instana.logger.info "Rack: Tracing disabled via config.  Not enabling middleware."
-      end
+    if ::Instana.config[:tracing][:enabled]
+      ::Instana.logger.debug "Instrumenting Rack"
+      ::Rails.configuration.middleware.insert 0, ::Instana::Rack
+    else
+      ::Instana.logger.info "Rack: Tracing disabled via config.  Not enabling middleware."
     end
-  else
-    module ::Instana
-      class Railtie < ::Rails::Railtie
-        initializer 'instana.rack' do |app|
-          # In Rails, let's use the Rails logger
-          ::Instana.logger = ::Rails.logger if ::Rails.logger
+  end
+else
+  module ::Instana
+    class Railtie < ::Rails::Railtie
+      initializer 'instana.rack' do |app|
+        # In Rails, let's use the Rails logger
+        ::Instana.logger = ::Rails.logger if ::Rails.logger
 
-          if ::Instana.config[:tracing][:enabled]
-            ::Instana.logger.debug "Instrumenting Rack"
-            app.config.middleware.insert 0, ::Instana::Rack
-          else
-            ::Instana.logger.info "Rack: Tracing disabled via config.  Not enabling middleware."
-          end
+        if ::Instana.config[:tracing][:enabled]
+          ::Instana.logger.debug "Instrumenting Rack"
+          app.config.middleware.insert 0, ::Instana::Rack
+        else
+          ::Instana.logger.info "Rack: Tracing disabled via config.  Not enabling middleware."
         end
       end
     end
