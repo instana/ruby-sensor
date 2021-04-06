e51b1f6c742ce31a78948697d11651957882f510
Hunter Madison <hunter.madison@instana.com>
2021-04-06 12:56:43 -0400

    Generate a snapshot for the Lambda function

--- lib/instana/setup.rb
+++ lib/instana/setup.rb
@@ -21,6 +21,7 @@ require 'instana/snapshot/fargate_process'
 require 'instana/snapshot/fargate_task'
 require 'instana/snapshot/fargate_container'
 require 'instana/snapshot/docker_container'
+require 'instana/snapshot/lambda_function'
 
 require 'instana/backend/host_agent_lookup'
 require 'instana/backend/host_agent_activation_observer'
