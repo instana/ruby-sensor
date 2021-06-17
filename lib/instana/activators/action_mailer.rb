1fd22f89ef64d3b3b774eb8254712217cf7a5899
Hunter Madison <hunter.madison@instana.com>
2021-06-17 08:42:28 -0400

    Monitor ActionMailer

---
+++ lib/instana/activators/action_mailer.rb
@@ -0,0 +1,22 @@
+# (c) Copyright IBM Corp. 2021
+# (c) Copyright Instana Inc. 2021
+
+module Instana
+  module Activators
+    class ActionMailer < Activator
+      def can_instrument?
+        defined?(::ActionMailer::Base)
+      end
+
+      def instrument
+        require 'instana/instrumentation/action_mailer'
+
+        ::ActionMailer::Base
+          .singleton_class
+          .prepend(Instana::Instrumentation::ActionMailer)
+
+        true
+      end
+    end
+  end
+end
