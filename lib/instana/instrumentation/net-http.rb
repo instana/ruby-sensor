39d579fd670a60c1cde258adc9b0e732d9aa0082
Federico Gimenez Molinelli <8161434+fgimenezm@users.noreply.github.com>
2021-07-01 20:27:27 -0300

    Update net-http.rb

--- lib/instana/instrumentation/net-http.rb
+++ lib/instana/instrumentation/net-http.rb
@@ -36,7 +36,7 @@ module Instana
         if request.uri
           uri_without_query = request.uri.dup.tap { |r| r.query = nil }
           kv_payload[:http][:url] = uri_without_query.to_s.gsub(/\?\z/, '')
-          kv_payload[:http][:params] = ::Instana.secrets.remove_from_query(request.uri.query)
+          kv_payload[:http][:params] = ::Instana.secrets.remove_from_query(request.uri.query) if !request.uri.query.nil?
         else
           if use_ssl?
             kv_payload[:http][:url] = "https://#{@address}:#{@port}#{request.path}"
