37dea232af142dce241b7dfa4eb84fc473e43c03
Ferenc Géczi <ferenc.geczi@ibm.com>
2023-06-12 10:00:00 +0000

    test(sidekiq): Allow Sidekiq 7 Config
    Signed-off-by: Ferenc Géczi <ferenc.geczi@ibm.com>

--- test/support/apps/sidekiq/worker.rb
+++ test/support/apps/sidekiq/worker.rb
@@ -12,12 +12,19 @@ require_relative 'jobs/sidekiq_job_2'
 ::Instana.logger.info "Booting instrumented sidekiq worker for tests."
 ::Sidekiq.logger.level = ::Logger::FATAL
 
+properties = {queues: ['important'], concurrency: 2}
+
+sidekiq_version = Gem::Specification.find_by_name('sidekiq').version
+config_or_options =
+  if sidekiq_version >= Gem::Version.new('7.0.0')
+    ::Sidekiq::Config.new(Sidekiq.default_configuration.merge!(properties))
+  else
+    ::Sidekiq.options.merge(properties)
+  end
+
 sidekiq_thread = Thread.new do
   launcher = ::Sidekiq::Launcher.new(
-    ::Sidekiq.options.merge(
-      queues: ['important'],
-      concurrency: 2
-    )
+    config_or_options
   )
   launcher.run
   Thread.current[:worker] = launcher
