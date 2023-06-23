350d232bcb75358ae766ac30c015f48fc3ac7ab6
Ferenc Géczi <ferenc.geczi@ibm.com>
2023-06-23 10:00:00 +0000

    feat: Add support for latest Dalli versions
    The `Dalli::Server` class has become deprecated.
    
    From `3.0.0` up until `3.1.12` the same logic is in
    `Dalli::Protocol::Binary`.
    
    Starting with `3.1.12`, the request handling from
    `Dalli::Protocol::Binary` has been moved to `Dalli::Protocol::Base`.
    
    The `@hostname` and `@port` fields are dropped between in `v3.1.1`,
    and only the `:hostname` and `:port` accessors  are available,
    but those have been available even earlier, definitely
    in the `2.0.0`, which is the oldest supported.
    
    Signed-off-by: Ferenc Géczi <ferenc.geczi@ibm.com>

--- lib/instana/activators/dalli.rb
+++ lib/instana/activators/dalli.rb
@@ -5,16 +5,22 @@ module Instana
   module Activators
     class Dalli < Activator
       def can_instrument?
-        defined?(::Dalli::Client) &&
-          defined?(::Dalli::Server) &&
+        defined?(::Dalli::Protocol::Base || defined?(::Dalli::Server)) &&
+          defined?(::Dalli::Client) &&
           Instana.config[:dalli][:enabled]
       end
 
       def instrument
         require 'instana/instrumentation/dalli'
-
+        dalli_version = Gem::Specification.find_by_name('dalli').version
         ::Dalli::Client.prepend ::Instana::Instrumentation::Dalli
-        ::Dalli::Server.prepend ::Instana::Instrumentation::DalliServer
+        if dalli_version < Gem::Version.new('3.0')
+          ::Dalli::Server.prepend ::Instana::Instrumentation::DalliRequestHandler
+        elsif dalli_version >= Gem::Version.new('3.0') && dalli_version < Gem::Version.new('3.1.3')
+          ::Dalli::Protocol::Binary.prepend ::Instana::Instrumentation::DalliRequestHandler
+        else
+          ::Dalli::Protocol::Base.prepend ::Instana::Instrumentation::DalliRequestHandler
+        end
 
         true
       end
