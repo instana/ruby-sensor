818f1ad8005b702a84c0aa094ba55eb5b635f8e2
Ferenc Géczi <ferenc.geczi@ibm.com>
2024-03-19 12:00:00 +0000

    test: Verify get_app_name with Rails and without application
    Signed-off-by: Ferenc Géczi <ferenc.geczi@ibm.com>

--- test/snapshot/ruby_process_test.rb
+++ test/snapshot/ruby_process_test.rb
@@ -10,5 +10,18 @@ class RubyProcessTest < Minitest::Test
 
     assert_equal Instana::Snapshot::RubyProcess::ID, snapshot[:name]
     assert_equal Process.pid.to_s, snapshot[:entityId]
+    assert_equal File.basename($0), snapshot[:data][:name]
+  end
+
+  def test_snapshot_with_rails_defined_but_no_rails_application
+    Object.send(:const_set, :Rails, Module.new {|mod| def respond_to?; return false; end})
+    subject = Instana::Snapshot::RubyProcess.new
+    snapshot = subject.snapshot
+
+    assert_equal Instana::Snapshot::RubyProcess::ID, snapshot[:name]
+    assert_equal Process.pid.to_s, snapshot[:entityId]
+    assert_equal File.basename($0), snapshot[:data][:name]
+  ensure
+    Object.send(:remove_const, :Rails)
   end
 end
