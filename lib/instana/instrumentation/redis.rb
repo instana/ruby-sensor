29f04247e088f7cb75e09b9f516a5581dde2912e
Ferenc Géczi <ferenc.geczi@ibm.com>
2024-03-22 12:00:00 +0000

    refactor: Eliminate code duplication from redis.rb
    Signed-off-by: Ferenc Géczi <ferenc.geczi@ibm.com>

--- lib/instana/instrumentation/redis.rb
+++ lib/instana/instrumentation/redis.rb
@@ -1,58 +1,49 @@
-# (c) Copyright IBM Corp. 2021
+# (c) Copyright IBM Corp. 2024
 # (c) Copyright Instana Inc. 2017
 
 module Instana
   module RedisInstrumentation
-    def call(*args, &block)
-      kv_payload = { redis: {} }
+    ORIGINAL_METHODS = {
+      :call => ::Redis::Client.instance_method(:call),
+      :call_pipeline => ::Redis::Client.instance_method(:call_pipeline)
+    }.freeze
+
+    def skip_instrumentation?
       dnt_spans = [:redis, :'resque-client', :'sidekiq-client']
+      !Instana.tracer.tracing? || dnt_spans.include?(::Instana.tracer.current_span.name) || !Instana.config[:redis][:enabled]
+    end
 
-      if !Instana.tracer.tracing? || dnt_spans.include?(::Instana.tracer.current_span.name) || !Instana.config[:redis][:enabled]
-        return super(*args, &block)
-      end
-
-      begin
-        ::Instana.tracer.log_entry(:redis)
-
-        begin
-          kv_payload[:redis][:connection] = "#{self.host}:#{self.port}"
-          kv_payload[:redis][:db] = db.to_s
-          kv_payload[:redis][:command] = args[0][0].to_s.upcase
-        rescue
-          nil
-        end
-
+    def call(*args, &block)
+      if skip_instrumentation?
         super(*args, &block)
-      rescue => e
-        ::Instana.tracer.log_info({ redis: {error: true} })
-        ::Instana.tracer.log_error(e)
-        raise
-      ensure
-        ::Instana.tracer.log_exit(:redis, kv_payload)
+      else
+        call_with_instana(args[0][0].to_s.upcase, ORIGINAL_METHODS[:call], *args, &block)
       end
     end
 
     def call_pipeline(*args, &block)
-      kv_payload = { redis: {} }
-      dnt_spans = [:redis, :'resque-client', :'sidekiq-client']
-
-      if !Instana.tracer.tracing? || dnt_spans.include?(::Instana.tracer.current_span.name) || !Instana.config[:redis][:enabled]
-        return super(*args, &block)
+      if skip_instrumentation?
+        super(*args, &block)
+      else
+        call_with_instana(args.first.is_a?(::Redis::Pipeline::Multi) ? 'MULTI' : 'PIPELINE', ORIGINAL_METHODS[:call_pipeline], *args, &block)
       end
+    end
+
+    def call_with_instana(*args, &block)
+      command, original_super, *original_args = *args
+      kv_payload = { redis: {} }
 
       begin
         ::Instana.tracer.log_entry(:redis)
 
-        pipeline = args.first
         begin
           kv_payload[:redis][:connection] = "#{self.host}:#{self.port}"
           kv_payload[:redis][:db] = db.to_s
-          kv_payload[:redis][:command] = pipeline.is_a?(::Redis::Pipeline::Multi) ? 'MULTI' : 'PIPELINE'
+          kv_payload[:redis][:command] = command
         rescue
           nil
         end
-
-        super(*args, &block)
+        original_super.bind(self).call(*original_args, &block)
       rescue => e
         ::Instana.tracer.log_info({ redis: {error: true} })
         ::Instana.tracer.log_error(e)
