27c66c84d23fdf519675036bc609ed14eab6f0c9
Ferenc Géczi <ferenc.geczi@ibm.com>
2024-03-25 12:00:00 +0000

    feat: Add full support for Redis 5
    Signed-off-by: Ferenc Géczi <ferenc.geczi@ibm.com>

--- lib/instana/instrumentation/redis.rb
+++ lib/instana/instrumentation/redis.rb
@@ -3,30 +3,66 @@
 
 module Instana
   module RedisInstrumentation
-    ORIGINAL_METHODS = {
-      :call => ::Redis::Client.instance_method(:call),
-      :call_pipeline => ::Redis::Client.instance_method(:call_pipeline)
-    }.freeze
+    if Gem::Specification.find_by_name('redis').version >= Gem::Version.new('5.0') && defined?(::RedisClient)
+      ORIGINAL_METHODS = {
+        :call_v => ::RedisClient.instance_method(:call_v),
+        :pipelined => ::RedisClient.instance_method(:pipelined),
+        :multi => ::RedisClient.instance_method(:multi)
+      }.freeze
+
+      def call_v(*args, &block)
+        if skip_instrumentation?
+          super(*args, &block)
+        else
+          call_with_instana(args[0][0].to_s.upcase, ORIGINAL_METHODS[:call_v], *args, &block)
+        end
+      end
+
+      def pipelined(*args, &block)
+        if skip_instrumentation?
+          super(*args, &block)
+        else
+          call_with_instana('PIPELINE', ORIGINAL_METHODS[:pipelined], *args, &block)
+        end
+      end
+
+      # Since, starting with 5.1 redis/client.rb:114:multi takes an unused default argument `watch: nil`
+      # but calls redis_client.rb:442:multi, which doesn't take any argument,
+      # here we have to take arguments but we should not use it.
+      def multi(*_, &block)
+        if skip_instrumentation?
+          super(&block)
+        else
+          call_with_instana('MULTI', ORIGINAL_METHODS[:multi], &block)
+        end
+      end
+
+    else
+      ORIGINAL_METHODS = {
+        :call => ::Redis::Client.instance_method(:call),
+        :call_pipeline => ::Redis::Client.instance_method(:call_pipeline)
+      }.freeze
+
+      def call(*args, &block)
+        if skip_instrumentation?
+          super(*args, &block)
+        else
+          call_with_instana(args[0][0].to_s.upcase, ORIGINAL_METHODS[:call], *args, &block)
+        end
+      end
+
+      def call_pipeline(*args, &block)
+        if skip_instrumentation?
+          super(*args, &block)
+        else
+          call_with_instana(args.first.is_a?(::Redis::Pipeline::Multi) ? 'MULTI' : 'PIPELINE', ORIGINAL_METHODS[:call_pipeline], *args, &block)
+        end
+      end
+    end
 
     def skip_instrumentation?
       dnt_spans = [:redis, :'resque-client', :'sidekiq-client']
       !Instana.tracer.tracing? || dnt_spans.include?(::Instana.tracer.current_span.name) || !Instana.config[:redis][:enabled]
-    end
-
-    def call(*args, &block)
-      if skip_instrumentation?
-        super(*args, &block)
-      else
-        call_with_instana(args[0][0].to_s.upcase, ORIGINAL_METHODS[:call], *args, &block)
-      end
-    end
-
-    def call_pipeline(*args, &block)
-      if skip_instrumentation?
-        super(*args, &block)
-      else
-        call_with_instana(args.first.is_a?(::Redis::Pipeline::Multi) ? 'MULTI' : 'PIPELINE', ORIGINAL_METHODS[:call_pipeline], *args, &block)
-      end
     end
 
     def call_with_instana(*args, &block)
