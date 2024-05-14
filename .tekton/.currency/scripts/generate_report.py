1da94dbaed8de64838ccd724ee419a5f68a385cf
Varsha GS <varsha.gs@ibm.com>
2024-05-14 12:40:05 +0530

    currency: use kubectl and bash to interact with tekton resources
    Signed-off-by: Varsha GS <varsha.gs@ibm.com>

--- .tekton/.currency/scripts/generate_report.py
+++ .tekton/.currency/scripts/generate_report.py
@@ -1,17 +1,15 @@
 # Standard Libraries
 import re
 from json import load
-from os import system
 
 # Third Party
 from requests import get
 from pandas import DataFrame
 from bs4 import BeautifulSoup
 
-JSON_FILE = "utils/table.json"
+JSON_FILE = "resources/table.json"
 REPORT_FILE = "docs/report.md"
-TEKTON_CI_OUT_SCRIPT = "scripts/get-tekton-ci-output.sh"
-TEKTON_CI_OUT_FILE = "utils/tekton-ci-output.txt"
+TEKTON_CI_OUT_FILE = "resources/tekton-ci-output.txt"
 API_V1_ENDPOINT = "https://rubygems.org/api/v1/versions/"
 LATEST_SUPPORTED_RUBY_VERSION = "3.3.0"
 
@@ -42,9 +40,6 @@ def get_upstream_version(dependency):
     return latest_version
 
 
-## Get the tekton ci output of the installed ruby dependencies
-system("bash " + TEKTON_CI_OUT_SCRIPT)
-
 with open(TEKTON_CI_OUT_FILE) as file:
     content = file.read()
 
@@ -66,55 +61,45 @@ def isUptodate(last_supported_version, latest_version):
 
     return up_to_date
 
+def main():
+    # Read the JSON file
+    with open(JSON_FILE) as file:
+        data = load(file)
 
-# Read the JSON file
-with open(JSON_FILE) as file:
-    data = load(file)
+    items = data["table"]
 
+    for item in items:
+        package = item["Package name"]
+        package = package.lower().replace("::", "-")
 
-items = data["table"]
+        if package == "net-http":
+            last_supported_version = LATEST_SUPPORTED_RUBY_VERSION
+            latest_version = get_latest_stable_ruby_version()
 
-for index in range(len(items)):
-    item = items[index]
-    package = item["package_name"]
-    package = package.lower().replace("::", "-")
-
-    if package == "net-http":
-        last_supported_version = LATEST_SUPPORTED_RUBY_VERSION
-        latest_version = get_latest_stable_ruby_version()
-
-    else:
-        latest_version = get_upstream_version(package)
-
-        if not package in ["rails lts", "rails-api"]:
-            last_supported_version = get_last_supported_version(package)
         else:
-            last_supported_version = latest_version
+            latest_version = get_upstream_version(package)
 
-    up_to_date = isUptodate(last_supported_version, latest_version)
+            if not package in ["rails lts", "rails-api"]:
+                last_supported_version = get_last_supported_version(package)
+            else:
+                last_supported_version = latest_version
 
-    item.update(
-        {
-            "last_supported_version": last_supported_version,
-            "latest_version": latest_version,
-            "up_to_date": up_to_date,
-        },
-    )
+        up_to_date = isUptodate(last_supported_version, latest_version)
 
-# Create a DataFrame from the list of dictionaries
-df = DataFrame(items)
-df.insert(len(df.columns) - 1, "cloud_native", df.pop("cloud_native"))
+        item.update(
+            {
+                "Last Supported Version": last_supported_version,
+                "Latest version": latest_version,
+                "Up-to-date": up_to_date,
+            },
+        )
 
-# Rename Columns
-df.columns = [
-    "Package name",
-    "Support Policy",
-    "Beta version",
-    "Last Supported Version",
-    "Latest version",
-    "Up-to-date",
-    "Cloud Native",
-]
+    # Create a DataFrame from the list of dictionaries
+    df = DataFrame(items)
+    df.insert(len(df.columns) - 1, "Cloud Native", df.pop("Cloud Native"))
 
-# Save the DataFrame as Markdown
-df.to_markdown(REPORT_FILE, index=False)
+    # Save the DataFrame as Markdown
+    df.to_markdown(REPORT_FILE, index=False)
+
+if __name__ == "__main__":
+    main()
--- .tekton/.currency/scripts/get-tekton-ci-output.sh
+++ .tekton/.currency/scripts/get-tekton-ci-output.sh
@@ -1,9 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-TEKTON_CI_OUT_FILE=utils/tekton-ci-output.txt
-
-# Empty the file
->$TEKTON_CI_OUT_FILE
+TEKTON_CI_OUT_FILE=resources/tekton-ci-output.txt
 
 taskrun_list=$(kubectl get taskrun --sort-by=.metadata.creationTimestamp | grep -v "pr\|Failed\|currency")
 
@@ -13,7 +10,8 @@ for library in "${!pattern_map1[@]}"
 do
     pattern=${pattern_map1[$library]}
 
-    successful_taskruns=( $(echo "${taskrun_list}" | grep "unittest-default-ruby-33-${pattern}\s" | awk '{print $1}') )
+    successful_taskruns=( $(echo "${taskrun_list}" | awk -v pattern="${pattern}" '{ if ($1 ~ /unittest-default-ruby-33-'"$pattern"'$/) { print $1 } }' ) )
+    # looping through the taskruns in reverse order to get the latest successful taskrun
     for ((i=${#successful_taskruns[@]}-1; i>=0; i--)); do
         pod_name=$(kubectl get taskrun "${successful_taskruns[$i]}" -o jsonpath='{.status.podName}')
         ci_output=$(kubectl logs ${pod_name} -c step-unittest | grep -o "Installing ${library} [^ ]*")
@@ -25,13 +23,13 @@ do
     echo ${latest_successful_taskrun_output} >> ${TEKTON_CI_OUT_FILE}
 done
 
-declare -A pattern_map2=( ["rails"]="rails-\w*-11" ["dalli"]="memcached-11" ["resque"]="unittest-redis-ruby-32-33-9" ["sidekiq"]="unittest-redis-ruby-32-33-18" )
+declare -A pattern_map2=( ["rails"]="rails-[[:alnum:]]*-11" ["dalli"]="memcached-11" ["resque"]="unittest-redis-ruby-32-33-9" ["sidekiq"]="unittest-redis-ruby-32-33-18" )
 
 for library in "${!pattern_map2[@]}"
 do
     pattern=${pattern_map2[$library]}
 
-    successful_taskruns=( $(echo "${taskrun_list}" | grep "${pattern}" | awk '{print $1}') )
+    successful_taskruns=( $(echo "${taskrun_list}" | awk -v pattern="${pattern}" '{ if ($0 ~ pattern) {print $1} }' ) )
     for ((i=${#successful_taskruns[@]}-1; i>=0; i--)); do
         pod_name=$(kubectl get taskrun "${successful_taskruns[$i]}" -o jsonpath='{.status.podName}')
         ci_output=$(kubectl logs ${pod_name} -c step-unittest | grep -o "Installing ${library} [^ ]*")
