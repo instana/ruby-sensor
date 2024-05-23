08ef8e0981e3e6b44037c3e2b0b0b208b4832058
Varsha GS <varsha.gs@ibm.com>
2024-05-23 13:25:17 +0530

    currency: use python kubernetes client to interact with tekton cluster
    Signed-off-by: Varsha GS <varsha.gs@ibm.com>

--- .tekton/.currency/scripts/generate_report.py
+++ .tekton/.currency/scripts/generate_report.py
@@ -6,12 +6,142 @@ from json import load
 from requests import get
 from pandas import DataFrame
 from bs4 import BeautifulSoup
+from kubernetes import client, config
 
 JSON_FILE = "resources/table.json"
 REPORT_FILE = "docs/report.md"
-TEKTON_CI_OUT_FILE = "resources/tekton-ci-output.txt"
 API_V1_ENDPOINT = "https://rubygems.org/api/v1/versions/"
-LATEST_SUPPORTED_RUBY_VERSION = "3.3.0"
+LATEST_SUPPORTED_RUBY_VERSION = "3.3.1"
+
+def filter_taskruns(taskrun_filter, taskruns):
+    filtered_taskruns = list(filter(taskrun_filter, taskruns))
+    filtered_taskruns.sort(
+        key=lambda tr: tr["metadata"]["creationTimestamp"], reverse=True
+    )
+
+    return filtered_taskruns
+
+
+def get_taskruns(namespace, task_name):
+    group = "tekton.dev"
+    version = "v1"
+    plural = "taskruns"
+
+    # access the custom resource from tekton
+    tektonV1 = client.CustomObjectsApi()
+    taskruns = tektonV1.list_namespaced_custom_object(
+        group,
+        version,
+        namespace,
+        plural,
+        label_selector=f"{group}/task={task_name}, triggers.tekton.dev/trigger=ruby-tracer-scheduled-pipeline-triggger",
+    )["items"]
+
+    return taskruns
+
+
+def process_taskrun_logs(
+    taskruns, core_v1_client, namespace, task_name, library, tekton_ci_output
+):
+    for tr in taskruns:
+        pod_name = tr["status"]["podName"]
+        taskrun_name = tr["metadata"]["name"]
+        logs = core_v1_client.read_namespaced_pod_log(
+            pod_name, namespace, container="step-unittest"
+        )
+        if "Installing" in logs:
+            print(
+                f"Retrieving container logs from the successful taskrun pod {pod_name} of taskrun {taskrun_name}.."
+            )
+            match = re.search(f"Installing ({library} [^\s]+)", logs)
+            tekton_ci_output += f"{match[1]}\n"
+            break
+        else:
+            print(
+                f"Unable to retrieve container logs from the successful taskrun pod {pod_name} of taskrun {taskrun_name}."
+            )
+    return tekton_ci_output
+
+
+def get_tekton_ci_output():
+    # config.load_kube_config()
+    config.load_incluster_config()
+
+    namespace = "default"
+    core_v1_client = client.CoreV1Api()
+
+    default_libraries_dict = {
+        "cuba": 1,
+        "excon": 4,
+        "graphql": 6,
+        "grpc": 7,
+        "rack": 10,
+        "rest-client": 11,
+        "roda": 13,
+        "sinatra": 16,
+    }
+
+    tekton_ci_output = ""
+    task_name = "ruby-tracer-unittest-default-libraries-task"
+    default_taskruns = get_taskruns(namespace, task_name)
+
+    for library, pattern in default_libraries_dict.items():
+        taskrun_filter = (
+            lambda tr: tr["metadata"]["name"].endswith(
+                f"unittest-default-ruby-33-{pattern}"
+            )
+            and tr["status"]["conditions"][0]["type"] == "Succeeded"
+        )
+        filtered_default_taskruns = filter_taskruns(taskrun_filter, default_taskruns)
+
+        tekton_ci_output = process_taskrun_logs(
+            filtered_default_taskruns,
+            core_v1_client,
+            namespace,
+            task_name,
+            library,
+            tekton_ci_output,
+        )
+
+    other_libraries_dict = {
+        "rails": {
+            "pattern": "rails-postgres-11",
+            "task_name": "ruby-tracer-unittest-rails-postgres-task",
+        },
+        "dalli": {
+            "pattern": "memcached-11",
+            "task_name": "ruby-tracer-unittest-memcached-libraries-task",
+        },
+        "resque": {
+            "pattern": "unittest-redis-ruby-32-33-9",
+            "task_name": "ruby-tracer-unittest-redis-libraries-task",
+        },
+        "sidekiq": {
+            "pattern": "unittest-redis-ruby-32-33-18",
+            "task_name": "ruby-tracer-unittest-redis-libraries-task",
+        },
+    }
+
+    for library, inner_dict in other_libraries_dict.items():
+        pattern = inner_dict["pattern"]
+        task_name = inner_dict["task_name"]
+        taskrun_filter = (
+            lambda tr: tr["metadata"]["name"].endswith(pattern)
+            and tr["status"]["conditions"][0]["type"] == "Succeeded"
+        )
+        other_taskruns = get_taskruns(namespace, task_name)
+        filtered_other_taskruns = filter_taskruns(taskrun_filter, other_taskruns)
+
+        tekton_ci_output = process_taskrun_logs(
+            filtered_other_taskruns,
+            core_v1_client,
+            namespace,
+            task_name,
+            library,
+            tekton_ci_output,
+        )
+
+    return tekton_ci_output
 
 
 def get_latest_stable_ruby_version():
@@ -40,15 +170,11 @@ def get_upstream_version(dependency):
     return latest_version
 
 
-with open(TEKTON_CI_OUT_FILE) as file:
-    content = file.read()
-
-
-def get_last_supported_version(dependency):
+def get_last_supported_version(tekton_ci_output, dependency):
     """get up-to-date supported version"""
     pattern = r" ([^\s]+)"
 
-    last_supported_version = re.search(dependency + pattern, content, flags=re.I | re.M)
+    last_supported_version = re.search(dependency + pattern, tekton_ci_output, flags=re.I | re.M)
 
     return last_supported_version[1]
 
@@ -66,6 +192,8 @@ def main():
     with open(JSON_FILE) as file:
         data = load(file)
 
+    tekton_ci_output = get_tekton_ci_output()
+
     items = data["table"]
 
     for item in items:
@@ -80,7 +208,7 @@ def main():
             latest_version = get_upstream_version(package)
 
             if not package in ["rails lts", "rails-api"]:
-                last_supported_version = get_last_supported_version(package)
+                last_supported_version = get_last_supported_version(tekton_ci_output, package)
             else:
                 last_supported_version = latest_version
 
