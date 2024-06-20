02a5caee02d13820b7d0f9860136f03bd8427fcb
Arjun Rajappa <Arjun.Rajappa@ibm.com>
2024-06-20 11:13:31 +0530

    Adding instrumentation to action view for rails version > 6.0
    Signed-off-by: Arjun Rajappa <Arjun.Rajappa@ibm.com>

--- lib/instana/instrumentation/action_view.rb
+++ lib/instana/instrumentation/action_view.rb
@@ -4,26 +4,51 @@
 module Instana
   module Instrumentation
     module ActionView
-      def render_partial(*args)
-        call_payload = {
-          render: {
-            type: :partial,
-            name: @options.is_a?(Hash) ? @options[:partial].to_s : 'Unknown'
+      module PartialRenderer
+        def render_partial(*args)
+          call_payload = {
+            render: {
+              type: :partial,
+              name: @options.is_a?(Hash) ? @options[:partial].to_s : 'Unknown'
+            }
           }
-        }
 
-        ::Instana::Tracer.trace(:render, call_payload) { super(*args) }
+          ::Instana::Tracer.trace(:render, call_payload) { super(*args) }
+        end
+
+        def render_collection(*args)
+          call_payload = {
+            render: {
+              type: :collection,
+              name: @path.to_s
+            }
+          }
+
+          ::Instana::Tracer.trace(:render, call_payload) { super(*args) }
+        end
+
+        def render_partial_template(*args)
+          call_payload = {
+            render: {
+              type: :partial,
+              name:  @options.is_a?(Hash) ? @options[:partial].to_s : 'Unknown'
+            }
+          }
+
+          ::Instana::Tracer.trace(:render, call_payload) { super(*args) }
+        end
       end
-
-      def render_collection(*args)
-        call_payload = {
-          render: {
-            type: :collection,
-            name: @path.to_s
+      module CollectionRenderer
+        def render_collection(*args)
+          call_payload = {
+            render: {
+              type: :collection,
+              name: @options.is_a?(Hash) ? @options[:partial].to_s : 'Unknown'
+            }
           }
-        }
 
-        ::Instana::Tracer.trace(:render, call_payload) { super(*args) }
+          ::Instana::Tracer.trace(:render, call_payload) { super(*args) }
+        end
       end
     end
   end
