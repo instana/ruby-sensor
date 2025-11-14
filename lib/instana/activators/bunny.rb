cef311949cb3ddbf53f409f3de55869d42a30dca
Arjun Rajappa <Arjun.Rajappa@ibm.com>
2025-11-14 12:51:58 +0530

    feat: base structure for bunny instrumentation
    Signed-off-by: Arjun Rajappa <Arjun.Rajappa@ibm.com>

---
+++ lib/instana/activators/bunny.rb
@@ -0,0 +1,22 @@
+# (c) Copyright IBM Corp. 2025
+
+module Instana
+  module Activators
+    class Bunny < Activator
+      def can_instrument?
+        defined?(::Bunny) &&
+          defined?(::Bunny::Queue) &&
+          defined?(::Bunny::Exchange) &&
+          ::Instana.config[:bunny][:enabled]
+      end
+
+      def instrument
+        require 'instana/instrumentation/bunny'
+
+        ::Bunny::Exchange.prepend(::Instana::Instrumentation::BunnyProducer)
+
+        true
+      end
+    end
+  end
+end
---
+++ lib/instana/instrumentation/bunny.rb
@@ -0,0 +1,38 @@
+# (c) Copyright IBM Corp. 2025
+
+module Instana
+  module Instrumentation
+    module BunnyProducer
+      def publish(payload, options = {})
+        if ::Instana.tracer.tracing?
+          exchange_name = name.empty? ? 'default' : name
+          routing_key = options[:routing_key] || ''
+
+          kvs = {
+            rabbitmq: {
+              sort: 'entry',
+              address: channel.connection.host,
+              key: routing_key,
+              exchange: exchange_name
+            }
+          }
+
+          ::Instana.tracer.in_span(:rabbitmq, attributes: kvs) do
+            # Inject trace context into message headers
+            options[:headers] ||= {}
+            options[:headers]['X-Instana-T'] = span.context.trace_id
+            options[:headers]['X-Instana-S'] = span.context.span_id
+            options[:headers]['X-Instana-L'] = span.context.level.to_s
+
+            super(payload, options)
+          end
+        else
+          super(payload, options)
+        end
+      rescue
+        ::Instana.logger.debug { "#{__method__}:#{File.basename(__FILE__)}:#{__LINE__}: #{e.message}" }
+        raise
+      end
+    end
+  end
+end
