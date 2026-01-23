527e356cbb3d91dabf8d59cf4dc268bece228d87
Arjun Rajappa <arjun.rajappa@ibm.com>
2026-01-23 18:12:52 +0530

    feat(span_stack): add tests for yml configuration read
    Signed-off-by: Arjun Rajappa <arjun.rajappa@ibm.com>

--- test/config_test.rb
+++ test/config_test.rb
@@ -121,4 +121,172 @@ class ConfigTest < Minitest::Test
   ensure
     ENV.delete('INSTANA_STACK_TRACE_LENGTH')
   end
+
+  # Tests for YAML configuration reading
+
+  def test_read_span_stack_config_from_yaml_with_both_values
+    yaml_content = <<~YAML
+      tracing:
+        global:
+          stack-trace: all
+          stack-trace-length: 25
+    YAML
+
+    File.write('test_stack_config.yaml', yaml_content)
+    ENV['INSTANA_CONFIG_PATH'] = 'test_stack_config.yaml'
+
+    subject = Instana::Config.new(logger: Logger.new('/dev/null'))
+
+    assert_equal 'all', subject[:back_trace][:stack_trace_level]
+    assert_equal 25, subject[:back_trace][:stack_trace_length]
+    assert_equal 'yaml', subject[:back_trace][:config_source]
+  ensure
+    File.unlink('test_stack_config.yaml') if File.exist?('test_stack_config.yaml')
+    ENV.delete('INSTANA_CONFIG_PATH')
+  end
+
+  def test_read_span_stack_config_from_yaml_with_error_level
+    yaml_content = <<~YAML
+      tracing:
+        global:
+          stack-trace: error
+          stack-trace-length: 15
+    YAML
+
+    File.write('test_stack_config.yaml', yaml_content)
+    ENV['INSTANA_CONFIG_PATH'] = 'test_stack_config.yaml'
+
+    subject = Instana::Config.new(logger: Logger.new('/dev/null'))
+
+    assert_equal 'error', subject[:back_trace][:stack_trace_level]
+    assert_equal 15, subject[:back_trace][:stack_trace_length]
+    assert_equal 'yaml', subject[:back_trace][:config_source]
+  ensure
+    File.unlink('test_stack_config.yaml') if File.exist?('test_stack_config.yaml')
+    ENV.delete('INSTANA_CONFIG_PATH')
+  end
+
+  def test_read_span_stack_config_from_yaml_with_none_level
+    yaml_content = <<~YAML
+      tracing:
+        global:
+          stack-trace: none
+    YAML
+
+    File.write('test_stack_config.yaml', yaml_content)
+    ENV['INSTANA_CONFIG_PATH'] = 'test_stack_config.yaml'
+
+    subject = Instana::Config.new(logger: Logger.new('/dev/null'))
+
+    assert_equal 'none', subject[:back_trace][:stack_trace_level]
+    assert_equal 30, subject[:back_trace][:stack_trace_length]
+    assert_equal 'yaml', subject[:back_trace][:config_source]
+  ensure
+    File.unlink('test_stack_config.yaml') if File.exist?('test_stack_config.yaml')
+    ENV.delete('INSTANA_CONFIG_PATH')
+  end
+
+  def test_read_span_stack_config_from_yaml_with_only_length
+    yaml_content = <<~YAML
+      tracing:
+        global:
+          stack-trace-length: 10
+    YAML
+
+    File.write('test_stack_config.yaml', yaml_content)
+    ENV['INSTANA_CONFIG_PATH'] = 'test_stack_config.yaml'
+
+    subject = Instana::Config.new(logger: Logger.new('/dev/null'))
+
+    assert_equal 'error', subject[:back_trace][:stack_trace_level]
+    assert_equal 10, subject[:back_trace][:stack_trace_length]
+    assert_equal 'yaml', subject[:back_trace][:config_source]
+  ensure
+    File.unlink('test_stack_config.yaml') if File.exist?('test_stack_config.yaml')
+    ENV.delete('INSTANA_CONFIG_PATH')
+  end
+
+  def test_read_span_stack_config_from_yaml_with_com_instana_tracing
+    yaml_content = <<~YAML
+      com.instana.tracing:
+        global:
+          stack-trace: all
+          stack-trace-length: 20
+    YAML
+
+    File.write('test_stack_config.yaml', yaml_content)
+    ENV['INSTANA_CONFIG_PATH'] = 'test_stack_config.yaml'
+
+    subject = Instana::Config.new(logger: Logger.new('/dev/null'))
+
+    assert_equal 'all', subject[:back_trace][:stack_trace_level]
+    assert_equal 20, subject[:back_trace][:stack_trace_length]
+    assert_equal 'yaml', subject[:back_trace][:config_source]
+  ensure
+    File.unlink('test_stack_config.yaml') if File.exist?('test_stack_config.yaml')
+    ENV.delete('INSTANA_CONFIG_PATH')
+  end
+
+  def test_read_span_stack_config_yaml_missing_global_section
+    yaml_content = <<~YAML
+      tracing:
+        filter:
+          deactivate: false
+    YAML
+
+    File.write('test_stack_config.yaml', yaml_content)
+    ENV['INSTANA_CONFIG_PATH'] = 'test_stack_config.yaml'
+
+    subject = Instana::Config.new(logger: Logger.new('/dev/null'))
+
+    # Should fall back to defaults
+    assert_equal 'error', subject[:back_trace][:stack_trace_level]
+    assert_equal 30, subject[:back_trace][:stack_trace_length]
+    assert_equal 'default', subject[:back_trace][:config_source]
+  ensure
+    File.unlink('test_stack_config.yaml') if File.exist?('test_stack_config.yaml')
+    ENV.delete('INSTANA_CONFIG_PATH')
+  end
+
+  def test_read_span_stack_config_yaml_takes_precedence_over_env
+    yaml_content = <<~YAML
+      tracing:
+        global:
+          stack-trace: all
+          stack-trace-length: 50
+    YAML
+
+    File.write('test_stack_config.yaml', yaml_content)
+    ENV['INSTANA_CONFIG_PATH'] = 'test_stack_config.yaml'
+    ENV['INSTANA_STACK_TRACE'] = 'none'
+    ENV['INSTANA_STACK_TRACE_LENGTH'] = '5'
+
+    subject = Instana::Config.new(logger: Logger.new('/dev/null'))
+
+    # YAML should take precedence
+    assert_equal 'all', subject[:back_trace][:stack_trace_level]
+    assert_equal 50, subject[:back_trace][:stack_trace_length]
+    assert_equal 'yaml', subject[:back_trace][:config_source]
+  ensure
+    File.unlink('test_stack_config.yaml') if File.exist?('test_stack_config.yaml')
+    ENV.delete('INSTANA_CONFIG_PATH')
+    ENV.delete('INSTANA_STACK_TRACE')
+    ENV.delete('INSTANA_STACK_TRACE_LENGTH')
+  end
+
+  def test_read_span_stack_config_invalid_yaml_falls_back_to_env
+    File.write('test_stack_config.yaml', "invalid: yaml: content: - [")
+    ENV['INSTANA_CONFIG_PATH'] = 'test_stack_config.yaml'
+    ENV['INSTANA_STACK_TRACE'] = 'all'
+
+    subject = Instana::Config.new(logger: Logger.new('/dev/null'))
+
+    # Should fall back to env vars
+    assert_equal 'all', subject[:back_trace][:stack_trace_level]
+    assert_equal 'env', subject[:back_trace][:config_source]
+  ensure
+    File.unlink('test_stack_config.yaml') if File.exist?('test_stack_config.yaml')
+    ENV.delete('INSTANA_CONFIG_PATH')
+    ENV.delete('INSTANA_STACK_TRACE')
+  end
 end
