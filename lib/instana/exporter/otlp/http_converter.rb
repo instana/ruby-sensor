f18cbe3c8ea5018751c17b46399081b5b5f866df
Arjun Rajappa <arjun.rajappa@ibm.com>
2026-06-04 18:55:49 +0530

    feat(otl-exporter): alter http_convertor to adapt to base_convertor struct
    Signed-off-by: Arjun Rajappa <arjun.rajappa@ibm.com>

--- lib/instana/exporter/otlp/http_converter.rb
+++ lib/instana/exporter/otlp/http_converter.rb
@@ -11,28 +11,14 @@ module Instana
       # Converter for HTTP spans to OTLP format
       # Handles conversion of HTTP-related spans with specific attributes
       class HttpConverter < BaseConverter
-        # Convert HTTP span to OTLP format
-        # @return [Hash] Converted HTTP span data in OTLP format
-        def convert
-          base_data = extract_common_attributes
-
-          # Add HTTP-specific attributes to the attributes array
-          http_attrs = extract_http_attributes
-          base_data[:attributes].concat(http_attrs) if http_attrs.any?
-
-          base_data
-        end
-
         private
 
-        # Extract HTTP-specific attributes in OTLP format
-        # @return [Array<Hash>] Array of OTLP key-value pairs for HTTP attributes
-        def extract_http_attributes
-          attributes = []
+        # Extract HTTP-specific attributes as plain key/value pairs
+        # @return [Hash] HTTP attributes
+        def convert_attributes
+          attributes = {}
           http_data = span[:data]&.[](:http) || {}
 
-          # Use semantic conventions constants for HTTP attributes
-          # Only add attributes that are actually present in Instana spans
           add_attribute(attributes, OpenTelemetry::SemanticConventions::Trace::HTTP_METHOD, http_data[:method])
           add_attribute(attributes, OpenTelemetry::SemanticConventions::Trace::HTTP_URL, http_data[:url])
           add_attribute(attributes, OpenTelemetry::SemanticConventions::Trace::HTTP_TARGET, http_data[:path])
@@ -40,7 +26,6 @@ module Instana
           add_attribute(attributes, OpenTelemetry::SemanticConventions::Trace::HTTP_SCHEME, extract_scheme(http_data[:url]))
           add_attribute(attributes, OpenTelemetry::SemanticConventions::Trace::HTTP_STATUS_CODE, http_data[:status])
           add_attribute(attributes, OpenTelemetry::SemanticConventions::Trace::HTTP_USER_AGENT, http_data.dig(:header, 'user-agent'))
-          # NOTE: request_content_length and response_content_length are not captured by Instana instrumentation
 
           attributes
         end
@@ -55,19 +40,6 @@ module Instana
           uri.scheme
         rescue URI::InvalidURIError
           nil
-        end
-
-        # Add an attribute to the attributes array if value is present
-        # @param attributes [Array<Hash>] The attributes array
-        # @param key [String] The attribute key
-        # @param value [Object] The attribute value
-        def add_attribute(attributes, key, value)
-          return unless value
-
-          attributes << {
-            key: key,
-            value: convert_attribute_value(value)
-          }
         end
       end
     end
