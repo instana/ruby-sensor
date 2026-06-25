efa52974ef906e238d047b81139b7473f35082fd
Arjun Rajappa <arjun.rajappa@ibm.com>
2026-06-25 07:15:05 +0530

    feat(otlp-exporter): add convertor to custom sdk
    Signed-off-by: Arjun Rajappa <arjun.rajappa@ibm.com>

--- test/exporter/otlp/custom_converter_test.rb
+++ test/exporter/otlp/custom_converter_test.rb
@@ -3,11 +3,45 @@
 require 'test_helper'
 require 'instana/exporter/otlp/custom_converter'
 
-# Stub test file for CustomConverter
-# TODO: Add comprehensive tests for custom span conversion
 class CustomConverterTest < Minitest::Test
-  def test_stub
-    # Placeholder test - implement actual tests when CustomConverter is fully implemented
-    skip 'CustomConverter is a stub - tests to be implemented'
+  def test_converts_instana_sdk_custom_span_attributes
+    span = Instana::Span.new(:my_custom_span)
+    span[:data] = { sdk: { name: 'my_custom_span', type: 'custom' } }
+    span.close
+
+    attributes = Instana::Exporter::Otlp::CustomConverter.new(span).convert.attributes
+
+    assert_equal 'custom', attributes['instana.span.type']
+    assert_equal 'my_custom_span', attributes['instana.sdk.name']
+    assert_equal 'custom', attributes['instana.sdk.type']
+  end
+
+  def test_converts_custom_tags
+    span = Instana::Span.new(:my_custom_span)
+    span.set_tag('user.id', 123)
+    span.set_tag('request.path', '/api/users')
+    span.close
+
+    attributes = Instana::Exporter::Otlp::CustomConverter.new(span).convert.attributes
+
+    assert_equal 123, attributes['user.id']
+    assert_equal '/api/users', attributes['request.path']
+  end
+
+  def test_converts_custom_tags_from_data_hash
+    span = Instana::Span.new(:my_custom_span)
+    span[:data] = {
+      sdk: {
+        custom: {
+          tags: { 'param1' => 'value1', 'param2' => 42 }
+        }
+      }
+    }
+    span.close
+
+    attributes = Instana::Exporter::Otlp::CustomConverter.new(span).convert.attributes
+
+    assert_equal 'value1', attributes['param1']
+    assert_equal 42, attributes['param2']
   end
 end
