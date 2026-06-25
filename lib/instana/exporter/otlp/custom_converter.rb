afbcd845a45bbd29b0a220e06c17637413d3e684
Arjun Rajappa <arjun.rajappa@ibm.com>
2026-06-25 07:15:05 +0530

    feat(otlp-exporter): add convertor to custom sdk
    Signed-off-by: Arjun Rajappa <arjun.rajappa@ibm.com>

--- lib/instana/exporter/otlp/custom_converter.rb
+++ lib/instana/exporter/otlp/custom_converter.rb
@@ -5,12 +5,25 @@
 module Instana
   module Exporter
     module Otlp
-      # Stub converter for custom spans to OTLP format
-      # This is a placeholder implementation for custom application-specific spans
-      # TODO: Implement full custom span conversion logic
+      # Converter for Instana SDK custom spans to OTLP format
       class CustomConverter < BaseConverter
-        # Convert custom span to OTLP format
-        # @return [Hash] Converted custom span data in OTLP format
+        def convert_attributes
+          attributes = {}
+          sdk_data = span[:data]&.[](:sdk) || {}
+
+          # Add standard Instana attributes
+          add_attribute(attributes, 'instana.span.type', 'custom')
+          add_attribute(attributes, 'instana.sdk.name', sdk_data[:name] || span[:n])
+          add_attribute(attributes, 'instana.sdk.type', sdk_data[:type])
+
+          # Add tags directly
+          tags = sdk_data.dig(:custom, :tags) || {}
+          tags.each do |key, value|
+            attributes[key] = value
+          end
+
+          attributes
+        end
       end
     end
   end
